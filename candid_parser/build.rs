c2595f17851267d5d6c6bc90ba4b05a638b579b4
chenyan-dfinity <yan.chen@dfinity.org>
2020-05-04 21:34:15 -0700

    test build

--- candid_parser/build.rs
+++ candid_parser/build.rs
@@ -3,7 +3,7 @@ extern crate lalrpop;
 fn main() {
     lalrpop::Configuration::new()
         .use_cargo_dir_conventions()
-        .force_build(true)
+        .force_build(false)
         .process()
         .unwrap();
 }
