169543b99fb8a918acdcce8102f786630c8cb229
chenyan-dfinity <yan.chen@dfinity.org>
2020-05-15 14:48:54 -0700

    parser codegen in source location

--- candid/build.rs
+++ candid/build.rs
@@ -1,7 +1,7 @@
 fn main() {
     lalrpop::Configuration::new()
-        .use_cargo_dir_conventions()
-        .force_build(true)
+        .generate_in_source_tree()
+        .emit_rerun_directives(true)
         .process()
         .unwrap();
 }
