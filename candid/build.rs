add20da6214be34c9e32db82e09a9164d7860948
chenyan-dfinity <yan.chen@dfinity.org>
2020-05-26 16:28:44 -0700

    fix

--- candid/build.rs
+++ candid/build.rs
@@ -1,7 +1,6 @@
 fn main() {
     lalrpop::Configuration::new()
         .use_cargo_dir_conventions()
-        .force_build(true)
         .process_file("src/parser/grammar.lalrpop")
         .unwrap();
 }
