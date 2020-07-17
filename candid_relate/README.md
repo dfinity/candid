cbb07bbb71ef7b14937bb76a4b741ae31d70a56f
Matthew Hammer <matthew@dfinity.org>
2020-07-17 14:55:20 -0600

    initial

---
+++ candid_relate/README.md
@@ -0,0 +1,11 @@
+# Candid Derive
+
+![](https://github.com/dfinity/candid/workflows/Rust/badge.svg)
+
+[Candid](https://github.com/dfinity/candid/tree/master/IDL.md) is an interface description language (IDL) for interacting with _canisters_ (also known as _services_ or _actors_) running on the Internet Computer.
+
+Macros implementation of `#[derive(CandidType)]` to convert Rust data types to Candid types. This crate should be considered as an implementation detail, and not be used directly, only via the `candid` crate.
+
+# Usage
+
+See [the docs here](https://docs.rs/candid_derive).
---
+++ candid_relate/src/lib.rs
@@ -0,0 +1,103 @@
+extern crate candid;
+
+use std::rc::Rc;
+
+pub type Type = candid::parser::types::IDLType;
+pub type Label = candid::parser::types::Label;
+pub type Field = candid::parser::types::TypeField;
+
+pub type Value = candid::parser::value::IDLValue;
+
+/// Represents editing an "input type" into an "output type".
+///
+/// only handle first-order types for now (no functions/services yet)
+pub enum TypeEdit<R> {
+    /// use "input type"
+    Skip,
+    /// ignore "input type" and use given type
+    Put(Type),
+    /// binary edit sequence
+    Seq(R, R),
+    /// edit param to an Opt type (no effect o/w)
+    Opt(R),
+    /// edit param to Vec type
+    Vec(R),
+    /// edit fields of record
+    Record(FieldsEdit<R>),
+    /// edit fields of variant
+    Variant(FieldsEdit<R>),
+}
+
+pub struct RcTypeEdit ( Rc<TypeEdit<RcTypeEdit>> );
+
+/// edit a field set: put some fields' types, drop others
+pub struct FieldsEdit<R> {
+    put: Vec<(Label, TypeEdit<R>)>,
+    drop: Vec<Label>,
+}
+
+pub type FieldsEditRc = FieldsEdit<RcTypeEdit>;
+
+pub fn fields_diff(fs1: &Vec<Field>, fs2: &Vec<Field>) -> FieldsEditRc {
+    // to do -- point-wise diff on common field names; drop field names that go away in the second set
+    drop((fs1, fs2));
+    unimplemented!()
+}
+
+pub fn rc_type_diff(t1: &Box<Type>, t2: &Box<Type>) -> RcTypeEdit {
+    RcTypeEdit(Rc::new(type_diff(&*t1, &*t2)))
+}
+
+pub fn type_diff(t1: &Type, t2: &Type) -> TypeEdit<RcTypeEdit> {
+    use candid::parser::types::IDLType::*;
+    match (t1, t2) {
+        (&PrincipalT, &PrincipalT) => {
+            TypeEdit::Skip
+        },
+        (&PrimT(ref p1), &PrimT(ref p2)) => {
+            if p1 == p2 {
+                TypeEdit::Skip
+            }
+            else {
+                TypeEdit::Put(t2.clone())
+            }
+        },
+        (&VarT(ref x), &VarT(ref y)) => {
+            if x == y {
+                TypeEdit::Skip
+            } else {
+                TypeEdit::Put(VarT(y.clone()))
+            }
+        },
+        (&OptT(ref t1), &OptT(ref t2)) => {
+            let r = rc_type_diff(&t1, &t2);
+            TypeEdit::Opt(r)
+        },
+        (&VecT(ref t1), &VecT(ref t2)) => {
+            let r = rc_type_diff(&t1, &t2);
+            TypeEdit::Vec(r)
+        },
+        (&RecordT(ref fs1), &RecordT(ref fs2)) => {
+            TypeEdit::Record(fields_diff(&fs1, &fs2))
+        },
+        (&VariantT(ref fs1), &VariantT(ref fs2)) => {
+            TypeEdit::Variant(fields_diff(&fs1, &fs2))
+        },
+        (_, _) => {
+            // by default, do the least incremental edit:
+            TypeEdit::Put(t2.clone())
+        }
+    }
+}
+
+
+pub struct ValueEdit {
+    // to do -- something similar to TypeEdit, but for values (not types)
+}
+
+
+pub fn value_diff(v1: &Value, v2: &Value, t: Type) -> ValueEdit {
+    // to do
+    drop((v1, v2, t));
+    unimplemented!()
+}
