c2f0810fca2f26df3d0baee456860ce7233fb39c
Hans Larsen <hans@larsen.online>
2020-07-30 15:47:42 -0700

    fix: do not run build.rs everytime we build
    This includes building crates that depend on candid.

--- candid/build.rs
+++ candid/build.rs
@@ -4,4 +4,8 @@ fn main() {
         .emit_rerun_directives(true)
         .process_file("src/parser/grammar.lalrpop")
         .unwrap();
+
+    // Make sure we only rerun the build script if we need to.
+    println!("cargo:rerun-if-changed=src/parser/grammar.lalrpop");
+    println!("cargo:rerun-if-changed=build.rs");
 }
