e664554d0ece01461c75723bd87c660c531d4870
Hans Larsen <hans@larsen.online>
2020-07-30 16:07:03 -0700

    test

--- candid/build.rs
+++ candid/build.rs
@@ -1,11 +1,13 @@
 fn main() {
+    // Make sure we only rerun the build script if we need to.
+    eprintln!("cargo:rerun-if-changed=src/parser/grammar.lalrpop");
+    eprintln!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=src/parser/grammar.lalrpop");
+    println!("cargo:rerun-if-changed=build.rs");
+
     lalrpop::Configuration::new()
         .use_cargo_dir_conventions()
         .emit_rerun_directives(true)
         .process_file("src/parser/grammar.lalrpop")
         .unwrap();
-
-    // Make sure we only rerun the build script if we need to.
-    eprintln!("cargo:rerun-if-changed=src/parser/grammar.lalrpop");
-    eprintln!("cargo:rerun-if-changed=build.rs");
 }
