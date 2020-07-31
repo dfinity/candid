c8782ca845190ccdf9669615f2a9f16a00f0b2c4
Prithvi Shahi <50885601+p-shahi@users.noreply.github.com>
2020-07-31 12:58:27 -0700

    New textual representation of Principals (#60)
    Overview
    The textual representation for Principals has changed from ic:xxxx to xxxxx-xxx
    
    Considerations
    This is a breaking change with regard to Principal textual format thus from_text and to_text will no longer work with the old format i.e ic:xxxx

--- candid/tests/value.rs
+++ candid/tests/value.rs
@@ -18,7 +18,7 @@ fn test_parser() {
     parse_check(
         "(variant { cons=record{ 42; variant { cons=record{43; variant { nil=record{} }} } } })",
     );
-    parse_check("(principal \"ic:caffee59\")");
+    parse_check("(principal \"w7x7r-cok77-xa\")");
 }
 
 #[test]
