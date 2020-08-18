b9f387e3ab7a2fd50e6a80bfe54515408211a97b
Yan Chen <48968912+chenyan-dfinity@users.noreply.github.com>
2020-08-17 22:08:36 -0700

    test suite for JS (#81)
    We add round tripping in the JS test generation, which may not work well with subtyping. Will revisit when we have more tests.
    
    ```
    assert blob "DIDL\00\01\7e\ff"             !: (bool) "bool: out of range";
    assert blob "DIDL\00\01\7d\00" == "(0)"         : (nat) "nat: 0";
    ```
    
    ```
    test('17 bool: out of range', () => {
      expect(
        () => IDL.decode([IDL.Bool], Buffer.from('4449444c00017eff', 'hex'))
      ).toThrow();
    });
    test('18 nat: 0', () => {
      expect(IDL.decode([IDL.Nat], Buffer.from('4449444c00017d00', 'hex'))).toEqual(
        [new BigNumber('0')]
      );
      expect(
        IDL.decode([IDL.Nat], IDL.encode([IDL.Nat], [new BigNumber('0')]))
      ).toEqual(IDL.decode([IDL.Nat], Buffer.from('4449444c00017d00', 'hex')));
    });
    ```

--- rust/candid/src/parser/test.rs
+++ rust/candid/src/parser/test.rs
@@ -11,7 +11,6 @@ pub struct Test {
     pub asserts: Vec<Assert>,
 }
 
-#[derive(Debug)]
 pub struct Assert {
     pub left: Input,
     pub right: Option<Input>,
@@ -20,14 +19,36 @@ pub struct Assert {
     pub desc: Option<String>,
 }
 
-#[derive(Debug)]
 pub enum Input {
     Text(String),
     Blob(Vec<u8>),
 }
 
+/// Generate assertions for host value
+pub struct HostTest {
+    pub desc: String,
+    pub asserts: Vec<HostAssert>,
+}
+pub enum HostAssert {
+    // The encoded bytes is not unique
+    Encode(IDLArgs, Vec<Type>, bool, Vec<u8>),
+    NotEncode(IDLArgs, Vec<Type>),
+    Decode(Vec<u8>, Vec<Type>, bool, IDLArgs),
+    NotDecode(Vec<u8>, Vec<Type>),
+}
+
+impl Assert {
+    pub fn desc(&self) -> String {
+        match &self.desc {
+            None => "",
+            Some(desc) => desc,
+        }
+        .to_string()
+    }
+}
+
 impl Input {
-    fn parse(&self, env: &TypeEnv, types: &[Type]) -> Result<IDLArgs> {
+    pub fn parse(&self, env: &TypeEnv, types: &[Type]) -> Result<IDLArgs> {
         match self {
             Input::Text(ref s) => s.parse::<IDLArgs>()?.annotate_types(env, types),
             Input::Blob(ref bytes) => Ok(IDLArgs::from_bytes_with_types(bytes, env, types)?),
@@ -52,6 +73,66 @@ impl std::str::FromStr for Test {
     }
 }
 
+impl HostTest {
+    pub fn from_assert(assert: &Assert, env: &TypeEnv, types: &[Type]) -> Self {
+        use HostAssert::*;
+        let types = types.to_vec();
+        let mut asserts = Vec::new();
+        match &assert.left {
+            Input::Text(s) => {
+                // Without type annotation, numbers are all of type int.
+                // Assertion may not pass.
+                let parsed = s.parse::<IDLArgs>();
+                if parsed.is_err() {
+                    let desc = format!("(skip) {}", assert.desc());
+                    return HostTest { desc, asserts };
+                }
+                let parsed = parsed.unwrap();
+                if !assert.pass && assert.right.is_none() {
+                    asserts.push(NotEncode(parsed, types));
+                } else {
+                    let bytes = parsed.to_bytes_with_types(env, &types).unwrap();
+                    asserts.push(Encode(parsed.clone(), types.clone(), true, bytes.clone()));
+                    // round tripping
+                    let vals = parsed.annotate_types(env, &types).unwrap();
+                    asserts.push(Decode(bytes, types, true, vals));
+                }
+                let desc = format!("(encode?) {}", assert.desc());
+                HostTest { desc, asserts }
+            }
+            Input::Blob(bytes) => {
+                let bytes = bytes.to_vec();
+                if !assert.pass && assert.right.is_none() {
+                    asserts.push(NotDecode(bytes, types));
+                } else {
+                    let args = IDLArgs::from_bytes_with_types(&bytes, env, &types).unwrap();
+                    asserts.push(Decode(bytes.clone(), types.clone(), true, args.clone()));
+                    // round tripping
+                    asserts.push(Encode(args, types.clone(), true, bytes.clone()));
+                    if let Some(right) = &assert.right {
+                        let expected = right.parse(env, &types).unwrap();
+                        if let Input::Blob(blob) = right {
+                            asserts.push(Decode(
+                                blob.to_vec(),
+                                types.clone(),
+                                true,
+                                expected.clone(),
+                            ));
+                        }
+                        if !assert.pass {
+                            asserts.push(Decode(bytes, types, assert.pass, expected));
+                        }
+                    }
+                }
+                HostTest {
+                    desc: assert.desc(),
+                    asserts,
+                }
+            }
+        }
+    }
+}
+
 pub fn check(test: Test) -> Result<()> {
     let mut env = TypeEnv::new();
     let prog = IDLProg {
@@ -61,7 +142,7 @@ pub fn check(test: Test) -> Result<()> {
     check_prog(&mut env, &prog)?;
     let mut count = 0;
     for (i, assert) in test.asserts.iter().enumerate() {
-        print!("Checking {}:{:?}...", i + 1, assert.desc);
+        print!("Checking {} {}...", i + 1, assert.desc());
         let mut types = Vec::new();
         for ty in assert.typ.iter() {
             types.push(env.ast_to_type(ty)?);
