c28a3f56c5ed823ba604e7d068b01770375050be
chenyan-dfinity <yan.chen@dfinity.org>
2020-09-14 21:07:45 -0700

    wasm for candid

---
+++ tools/wasm/src/lib.rs
@@ -0,0 +1,36 @@
+use cfg_if::cfg_if;
+use wasm_bindgen::prelude::*;
+use candid::{IDLProg, check_prog, TypeEnv};
+
+cfg_if! {
+    // When the `console_error_panic_hook` feature is enabled, we can call the
+    // `set_panic_hook` function at least once during initialization, and then
+    // we will get better error messages if our code ever panics.
+    //
+    // For more details see
+    // https://github.com/rustwasm/console_error_panic_hook#readme
+    if #[cfg(feature = "console_error_panic_hook")] {
+        pub use console_error_panic_hook::set_once as set_panic_hook;
+    } else {
+        #[inline]
+        pub fn set_panic_hook() {}
+    }
+}
+
+cfg_if! {
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    if #[cfg(feature = "wee_alloc")] {
+        extern crate wee_alloc;
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
+
+#[wasm_bindgen]
+pub fn did_to_js(prog: String) -> Option<String> {
+    let ast = prog.parse::<IDLProg>().ok()?;
+    let mut env = TypeEnv::new();
+    let actor = check_prog(&mut env, &ast).ok()?;
+    Some(candid::bindings::javascript::compile(&env, &actor))
+}
