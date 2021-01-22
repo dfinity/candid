94f5339a0e1c0d932928018da1eafcba31a8feff
chenyan-dfinity <yan.chen@dfinity.org>
2021-01-21 16:19:24 -0800

    depth and size config

--- rust/candid/src/parser/random.rs
+++ rust/candid/src/parser/random.rs
@@ -3,16 +3,16 @@ use super::value::{IDLArgs, IDLField, IDLValue};
 use crate::types::{Field, Type};
 use crate::Result;
 use arbitrary::Unstructured;
+use std::cell::Cell;
 use std::collections::HashMap;
 
 const MAX_DEPTH: usize = 20;
 
-#[derive(Clone, Copy)]
 pub struct GenConfig {
     range: (usize, usize),
     text: &'static str, // regex or pattern
-    depth: usize,
-    width: usize,
+    depth: Cell<usize>,
+    size: Cell<usize>,
 }
 
 pub struct GenTable(HashMap<Type, GenConfig>);
@@ -22,15 +22,26 @@ impl Default for GenConfig {
         GenConfig {
             range: (0, 100),
             text: "ascii",
-            depth: 20,
-            width: 20,
+            depth: Cell::new(10),
+            size: Cell::new(100),
         }
     }
 }
 impl GenConfig {
-    fn dec_depth(self) -> Self {
-        let depth = if self.depth == 0 { 0 } else { self.depth - 1 };
-        GenConfig { depth, ..self }
+    fn dec(&self, dsize: usize) {
+        let size = self.size.get();
+        let depth = self.depth.get();
+        if size < dsize {
+            self.size.set(0)
+        } else {
+            self.size.set(size - dsize)
+        };
+        if depth > 0 {
+            self.depth.set(depth - 1);
+        }
+    }
+    fn inc(&self) {
+        self.depth.set(self.depth.get() + 1);
     }
 }
 
@@ -39,7 +50,7 @@ impl IDLArgs {
         let mut args = Vec::new();
         for t in types.iter() {
             let config = GenConfig::default();
-            let v = IDLValue::any(u, config, env, t)?;
+            let v = IDLValue::any(u, &config, env, t)?;
             args.push(v);
         }
         Ok(IDLArgs { args })
@@ -72,29 +83,39 @@ impl TypeEnv {
             _ => 1,
         }
     }
-    /// lower bound for size
-    fn size(&self, limit: usize, t: &Type) -> usize {
+    /// lower and upper bound for size
+    fn size(&self, limit: usize, t: &Type) -> (usize, usize) {
         use Type::*;
         if limit == 0 {
-            return 1;
+            return (1, 1);
         }
         match t {
             Var(id) => {
                 let ty = self.rec_find_type(id).unwrap();
-                self.depth(limit, &ty)
+                self.size(limit, &ty)
             }
-            Empty => 0,
-            Opt(t) => self.size(limit - 1, t),
-            Vec(t) => self.size(limit - 1, t),
-            Record(fs) => fs
-                .iter()
-                .map(|Field { ty, .. }| self.size(limit - 1, ty))
-                .sum(),
-            Variant(fs) => fs
-                .iter()
-                .map(|Field { ty, .. }| self.size(limit - 1, ty))
-                .min(),
-            _ => 1,
+            Empty => (0, 0),
+            Opt(t) => {
+                let (_, u) = self.size(limit - 1, t);
+                (1, 1 + u)
+            }
+            Vec(t) => {
+                let (_, u) = self.size(limit - 1, t);
+                (1, 1 + u * 2)
+            }
+            Record(fs) => {
+                let iter = fs.iter().map(|Field { ty, .. }| self.size(limit - 1, ty));
+                let l: usize = iter.clone().map(|e| e.0).sum();
+                let r: usize = iter.map(|e| e.1).sum();
+                (1 + l, 1 + r)
+            }
+            Variant(fs) => {
+                let iter = fs.iter().map(|Field { ty, .. }| self.size(limit - 1, ty));
+                let l = std::cmp::max(1, iter.clone().map(|e| e.0).min().unwrap());
+                let r = iter.map(|e| e.1).max().unwrap();
+                (1 + l, 1 + r)
+            }
+            _ => (1, 1),
         }
     }
 }
@@ -118,10 +139,13 @@ fn arbitrary_variant(u: &mut Unstructured, weight: &[usize]) -> Result<usize> {
 }
 
 impl IDLValue {
-    pub fn any(u: &mut Unstructured, config: GenConfig, env: &TypeEnv, ty: &Type) -> Result<Self> {
+    pub fn any(u: &mut Unstructured, config: &GenConfig, env: &TypeEnv, ty: &Type) -> Result<Self> {
+        config.dec(1);
+        println!("{} {}", config.depth.get(), config.size.get());
         Ok(match ty {
             Type::Var(id) => {
                 let ty = env.rec_find_type(id)?;
+                config.inc();
                 Self::any(u, config, env, &ty)?
             }
             Type::Null => IDLValue::Null,
@@ -146,23 +170,28 @@ impl IDLValue {
             Type::Text => IDLValue::Text(u.arbitrary()?),
             Type::Principal => IDLValue::Principal(crate::Principal::anonymous()),
             Type::Opt(t) => {
-                let sizes = if config.depth == 0 {
+                let depths = if config.depth.get() == 0 || config.size.get() == 0 {
                     [1, 0]
                 } else {
                     [1, env.depth(MAX_DEPTH, t)]
                 };
-                let idx = arbitrary_variant(u, &sizes)?;
+                let idx = arbitrary_variant(u, &depths)?;
                 if idx == 0 {
                     IDLValue::None
                 } else {
-                    IDLValue::Opt(Box::new(Self::any(u, config.dec_depth(), env, t)?))
+                    let res = IDLValue::Opt(Box::new(Self::any(u, config, env, t)?));
+                    config.inc();
+                    res
                 }
             }
             Type::Vec(t) => {
-                let len = u.int_in_range(0..=config.width)?;
+                let (lower, _) = env.size(MAX_DEPTH, t);
+                let max_len = config.size.get() / (lower * 2);
+                let len = u.int_in_range(0..=max_len)?;
                 let mut vec = Vec::with_capacity(len);
                 for _ in 0..len {
-                    let e = Self::any(u, config.dec_depth(), env, t)?;
+                    let e = Self::any(u, config, env, t)?;
+                    config.inc();
                     vec.push(e);
                 }
                 IDLValue::Vec(vec)
@@ -170,7 +199,8 @@ impl IDLValue {
             Type::Record(fs) => {
                 let mut res = Vec::new();
                 for Field { id, ty } in fs.iter() {
-                    let val = Self::any(u, config.dec_depth(), env, ty)?;
+                    let val = Self::any(u, config, env, ty)?;
+                    config.inc();
                     res.push(IDLField {
                         id: id.clone(),
                         val,
@@ -180,7 +210,7 @@ impl IDLValue {
             }
             Type::Variant(fs) => {
                 let choices = fs.iter().map(|Field { ty, .. }| env.depth(MAX_DEPTH, ty));
-                let sizes: Vec<_> = if config.depth == 0 {
+                let sizes: Vec<_> = if config.depth.get() == 0 || config.size.get() == 0 {
                     let min = choices.clone().min().unwrap_or(0);
                     choices.map(|d| if d > min { 0 } else { d }).collect()
                 } else {
@@ -188,7 +218,8 @@ impl IDLValue {
                 };
                 let idx = arbitrary_variant(u, &sizes)?;
                 let Field { id, ty } = &fs[idx];
-                let val = Self::any(u, config.dec_depth(), env, ty)?;
+                let val = Self::any(u, config, env, ty)?;
+                config.inc();
                 let field = IDLField {
                     id: id.clone(),
                     val,
