942bbb6dbf7edcce18c4d529e0ed571b481a735b
Joachim Breitner <mail@joachim-breitner.de>
2021-01-27 11:29:55 +0100

    Coq: Model subtype check on decoding
    this models #168 in Coq. So far so good.
    
    The crucial bit will be connecting this to the IDL Soundness
    formalization. That requires
     * adding a function reference type
     * proving a subtyping-compositionality lemma, namely
       ```
       If t1 <: t2 and s1 in t1 <: s2 in t2 then s1 <: s2.
       ```
    
    That will be no small untertaking. I will probably remove the other
    variants in the Coq file then?

--- coq/MiniCandid.v
+++ coq/MiniCandid.v
@@ -11,6 +11,8 @@ Require Import Coq.Relations.Relation_Operators.
 Require Import Coq.Relations.Relation_Definitions.
 Require Import Coq.Relations.Operators_Properties.
 
+Require Import Coq.Logic.Decidable.
+
 Set Bullet Behavior "Strict Subproofs".
 Set Default Goal Selector "!".
 
@@ -87,10 +89,10 @@ Here things are simple and inductive.
 Reserved Infix "<:" (at level 80, no associativity).
 CoInductive Subtype : T -> T -> Prop :=
   | ReflST :
-    case reflSL,
+    case reflST,
     forall t, t <: t
   | NatIntST :
-    case natIntSL,
+    case natIntST,
     NatT <: IntT
   | NullOptST :
     case nullOptST,
@@ -198,7 +200,7 @@ Proof.
     inversion H0; subst; clear H0; simpl in H; inversion H.
     econstructor. named_econstructor; [constructor|named_constructor].
   }
-  [optHT_reflSL]: {
+  [optHT_reflST]: {
     specialize (IHHvT t (ReflST _ _)).
     destruct IHHvT as [v2 Hv2].
     eexists. named_econstructor; try eassumption.
@@ -232,7 +234,7 @@ Proof.
   inversion H2; subst; clear H2;
     name_cases;
     try (constructor; easy).
-  [natIntSL_constituentOptST]: {
+  [natIntST_constituentOptST]: {
     named_constructor.
     - assumption.
     - eapply Hyp; [named_econstructor | assumption].
@@ -268,10 +270,10 @@ This is the variant with the opportunistic `t <: opt t'` rule.
 Reserved Infix "<:" (at level 80, no associativity).
 CoInductive Subtype : T -> T -> Prop :=
   | ReflST :
-    case reflSL,
+    case reflST,
     forall t, t <: t
   | NatIntST :
-    case natIntSL,
+    case natIntST,
     NatT <: IntT
   | OptST :
     case optST,
@@ -284,6 +286,212 @@ CoInductive Subtype : T -> T -> Prop :=
     case reservedST,
     forall t, t <: ReservedT
 where "t1 <: t2" := (Subtype t1 t2).
+
+Module SubtypeCheck.
+(*
+This is the variant with the subtyping check during decoding.
+*)
+
+(*
+Subtyping is undecidable, at least the way we model it in Coq.
+So let’s pretend it is.
+*)
+Axiom subtyping_decidable:
+  forall t1 t2, {t1 <: t2} + { ~(t1 <: t2) }.
+Infix "<:?" := subtyping_decidable (at level 80, no associativity).
+
+(*
+The spec defines the coercion function as indexed by the subtyping relation.
+But that relation is coinductive, so Coq will not allow that.
+We thus define the function by recursion on the value.
+
+We use NullV on the RHS of invalid cases.
+*)
+
+Function coerce (t1 : T) (t2 : T) (v1 : V) : V :=
+  match v1, t1, t2 with
+  | NatV n, NatT, NatT => NatV n
+  | IntV n, IntT, IntT => IntV n
+  | NatV n, NatT, IntT => IntV (Z.of_nat n)
+
+  | SomeV v, OptT t1, OptT t2 =>
+    if t1 <:? t2
+    then SomeV (coerce t1 t2 v)
+    else NullV
+  
+  (* This is the rule we would like to have, but 
+     in order to please the termination checker,
+     we have to duplicate all non-opt rules in their opt variant
+  | v, t1, OptT t2 =>
+    if not (is_opt_like_type t) && t <: t2
+    then SomeV (coerce v t1 t2)
+  *)
+  | NatV n, NatT, OptT NatT => SomeV (NatV n)
+  | IntV n, IntT, OptT IntT => SomeV (IntV n)
+  | NatV n, NatT, OptT IntT => SomeV (IntV (Z.of_nat n))
+
+  | v, t, ReservedT => ReservedV
+
+  (* Failure is NullV. This also subsumes “valid” rules for NullV *)
+  | _, _, _ => NullV
+  end.
+
+
+(* Let’s try to create a suitable induction principle for this function *)
+Lemma coerce_nice_ind:
+  forall (P : T -> T -> V -> V -> Prop),
+  (case natC, forall n, P NatT NatT (NatV n) (NatV n)) ->
+  (case intC, forall n, P IntT IntT (IntV n) (IntV n)) ->
+  (case natIntC, forall n, P NatT IntT (NatV n) (IntV (Z.of_nat n))) ->
+  (case nullC, P NullT NullT NullV NullV) ->
+  (case nullOptC, forall t, P NullT (OptT t) NullV NullV) ->
+  (case optNullC, forall t1 t2, P (OptT t1) (OptT t2) NullV NullV) ->
+  (case optSomeC, forall t1 t2 v1 v2,
+    t1 <: t2 ->
+    P t1 t2 v1 v2 ->
+    P (OptT t1) (OptT t2) (SomeV v1) (SomeV v2)) ->
+  (case opportunisticOptC, forall t1 t2 v1,
+    ~ (t1 <: t2) ->
+    P (OptT t1) (OptT t2) (SomeV v1) NullV) ->
+  (case reservedOptC,
+    forall t, P ReservedT (OptT t) ReservedV NullV) ->
+  (case constituentOptC,
+    forall t1 t2 v1 v2,
+    is_opt_like_type t1 = false ->
+    is_opt_like_type t2 = false ->
+    t1 <: t2 ->
+    P t1 t2 v1 v2 ->
+    P t1 (OptT t2) v1 (SomeV v2)) ->
+  (case opportunisticConstituentOptC,
+    forall t1 t2 v1,
+    is_opt_like_type t1 = false ->
+    is_opt_like_type t2 = true \/ ~ (t1 <: t2) ->
+    P t1 (OptT t2) v1 NullV) ->
+  (case reservedC, forall t v, v :: t -> P t ReservedT v ReservedV) ->
+  (forall t1 t2 v1, t1 <: t2 -> v1 :: t1 -> P t1 t2 v1 (coerce t1 t2 v1)).
+Proof.
+  intros P.
+  intros NatC IntC NatIntC NullC NullOptC OptNullC OptSomeC OpportunisticOptC ReservedOptC ConstituentOptC OpportunisticConstituentOptC ReservedC.
+  intros t1 t2 v1 HST HHT.
+  revert t2 HST.
+  induction HHT; name_cases.
+  [natHT]: {
+    intros. 
+    inversion HST; subst; clear HST; name_cases.
+    [reflST]: { apply NatC; clear_names. }
+    [natIntST]: { apply NatIntC; clear_names. }
+    [optST]: {
+      destruct (is_opt_like_type t0) eqn:His_opt_like.
+      * destruct t0; inversion His_opt_like; simpl; clear His_opt_like;
+        apply OpportunisticConstituentOptC; clear_names; simpl; intuition.
+      * destruct (subtyping_decidable NatT t0).
+        + destruct t0; inversion s; subst; clear s; inversion His_opt_like; clear His_opt_like.
+          - apply ConstituentOptC; clear_names; simpl; intuition; named_constructor.
+          - apply ConstituentOptC; clear_names; simpl; intuition; named_constructor.
+        + destruct t0; inversion His_opt_like; clear His_opt_like.
+          - contradict n0. named_constructor.
+          - contradict n0. named_constructor.
+          - apply OpportunisticConstituentOptC; clear_names; simpl; intuition.
+    }
+    [reservedST]: { apply ReservedC; clear_names. named_constructor. }
+  }
+  [intHT]: {
+    intros. 
+    inversion HST; subst; clear HST; name_cases.
+    [reflST]: { apply IntC; clear_names. }
+    [optST]: {
+      destruct (is_opt_like_type t0) eqn:His_opt_like.
+      * destruct t0; inversion His_opt_like; simpl; clear His_opt_like;
+        apply OpportunisticConstituentOptC; clear_names; simpl; intuition.
+      * destruct (subtyping_decidable IntT t0).
+        + destruct t0; inversion s; subst; clear s; inversion His_opt_like; clear His_opt_like.
+          - apply ConstituentOptC; clear_names; simpl; intuition; named_constructor.
+        + destruct t0; inversion His_opt_like; clear His_opt_like.
+          - apply OpportunisticConstituentOptC; clear_names; simpl; intuition.
+          - contradict n0. named_constructor.
+          - apply OpportunisticConstituentOptC; clear_names; simpl; intuition.
+    }
+    [reservedST]: { apply ReservedC; clear_names. named_constructor. }
+  }
+  [nullHT]: {
+    intros.
+    inversion HST; subst; clear HST; name_cases.
+    [reflST]: { apply NullC; clear_names. }
+    [optST]: { apply NullOptC; clear_names. }
+    [reservedST]: { apply ReservedC; clear_names. named_constructor. }
+  }
+  [nullOptHT]: {
+    intros.
+    inversion HST; subst; clear HST; name_cases.
+    [reflST]: { apply OptNullC; clear_names. }
+    [optST]: { apply OptNullC; clear_names. }
+    [reservedST]: { apply ReservedC; clear_names. named_constructor. }
+  }
+  [optHT]: {
+    intros.
+    inversion HST; subst; clear HST; name_cases.
+    [reflST]: {
+      simpl.
+      destruct (t <:? t) as [HST | HNoST].
+      * apply OptSomeC; clear_names; intuition.
+      * contradict HNoST. apply ReflST; clear_names.
+    }
+    [optST]: {
+      simpl. 
+      destruct (t <:? t0) as [HST | HNoST].
+      * apply OptSomeC; clear_names; intuition.
+      * apply OpportunisticOptC; clear_names; intuition.
+    }
+    [reservedST]: { apply ReservedC; clear_names. named_constructor; assumption. }
+  }
+  [reservedHT]: { 
+    intros.
+    inversion HST; subst; clear HST; name_cases.
+    [reflST]: { apply ReservedC; clear_names. named_constructor. }
+    [optST]: { apply ReservedOptC; clear_names. }
+    [reservedST]: { apply ReservedC; clear_names.  named_constructor. }
+  }
+Qed.
+
+Lemma coerce_roundtrip:
+  forall t1 v1,
+  v1 :: t1 ->
+  coerce t1 t1 v1 = v1.
+Proof.
+  enough (forall t1 t2 v1,
+    t1 <: t2 -> v1 :: t1 -> t2 = t1 ->
+    coerce t1 t2 v1 = v1)
+    by (intros; apply H; intuition; try apply ReflST; clear_names).
+  apply (coerce_nice_ind (fun t1 t2 v1 v2 => t2 = t1 -> v2 = v1));
+    intros; name_cases; subst; simpl in *; try congruence.
+  Show Existentials.
+  [optSomeC]: {apply f_equal. apply H0. congruence. }
+  [opportunisticOptC]: {
+    inversion H0; subst; clear H0. contradiction H; apply ReflST; clear_names.
+  }
+  * (* why is this not named? *)
+    inversion H; subst; clear H. reflexivity.
+Qed.
+
+Lemma coerce_well_defined:
+  forall t1 t2 v1,
+  t1 <: t2 -> v1 :: t1 ->
+  coerce t1 t2 v1 :: t2.
+Proof.
+  apply coerce_nice_ind with (P := fun t1 t2 v1 v2 => v2 :: t2); intros; name_cases;
+     named_constructor; assumption.
+Qed.
+
+
+
+End SubtypeCheck.
+
+
+Module TypeErasure.
+(*
+This is the variant with type erasure, and dynamic opportnistic decoding.
+Not safe in the higher-order case.
+*)
 
 (*
 The coercion relation is not strictly positive, and thus can’t be an inductive
@@ -317,6 +525,7 @@ Function coerce (v1 : V) (t : T) : option V :=
   | NatV n, OptT NatT => recover (Some (NatV n))
   | IntV n, OptT IntT => recover (Some (IntV n))
   | NatV n, OptT IntT => recover (Some (IntV (Z.of_nat n)))
+  
   (* OptT never fails (this subsumes NullV and ReservedV) *)
   | v, OptT _ => Some NullV
 
@@ -510,7 +719,7 @@ Proof.
   [intHT_optST]: {
     destruct t2; eexists; reflexivity.
   }
-  [optHT_reflSL]: {
+  [optHT_reflST]: {
     specialize (IHHvT t (ReflST _ _)).
     destruct IHHvT as [v2 Hv2].
     eexists. unfold Coerces in *. simpl. rewrite Hv2. reflexivity.
@@ -538,5 +747,8 @@ Proof.
     name_cases;
     try (constructor; easy).
 Qed.
+
+End TypeErasure.
+
 
 End OpportunisticDecoding.
