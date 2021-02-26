5b8f5cb38752313dc6a73aeb6d7276c644b0dac0
chenyan-dfinity <yan.chen@dfinity.org>
2021-02-26 09:19:50 -0800

    refactor

--- rust/candid/src/types/subtype.rs
+++ rust/candid/src/types/subtype.rs
@@ -30,49 +30,26 @@ pub fn subtype(env: &TypeEnv, t1: &Type, t2: &Type) -> bool {
         }
         (Record(fs1), Record(fs2)) => {
             let fields: HashMap<_, _> = fs1.iter().map(|Field { id, ty }| (id, ty)).collect();
-            for Field { id, ty: ty2 } in fs2.iter() {
-                match fields.get(id) {
-                    Some(ty1) => {
-                        if !subtype(env, ty1, ty2) {
-                            return false;
-                        }
-                    }
-                    None => {
-                        if !subtype(env, &Opt(Box::new(Empty)), ty2) {
-                            return false;
-                        }
-                    }
-                }
-            }
-            true
+            fs2.iter()
+                .all(|Field { id, ty: ty2 }| match fields.get(id) {
+                    Some(ty1) => subtype(env, ty1, ty2),
+                    None => subtype(env, &Opt(Box::new(Empty)), ty2),
+                })
         }
         (Variant(fs1), Variant(fs2)) => {
             let fields: HashMap<_, _> = fs2.iter().map(|Field { id, ty }| (id, ty)).collect();
-            for Field { id, ty: ty1 } in fs1.iter() {
-                match fields.get(id) {
-                    Some(ty2) => {
-                        if !subtype(env, ty1, ty2) {
-                            return false;
-                        }
-                    }
-                    None => return false,
-                }
-            }
-            true
+            fs1.iter()
+                .all(|Field { id, ty: ty1 }| match fields.get(id) {
+                    Some(ty2) => subtype(env, ty1, ty2),
+                    None => false,
+                })
         }
         (Service(ms1), Service(ms2)) => {
             let meths: HashMap<_, _> = ms2.iter().map(|(name, ty)| (name, ty)).collect();
-            for (name, ty1) in ms1.iter() {
-                match meths.get(name) {
-                    Some(ty2) => {
-                        if !subtype(env, ty1, ty2) {
-                            return false;
-                        }
-                    }
-                    None => return false,
-                }
-            }
-            true
+            ms1.iter().all(|(name, ty1)| match meths.get(name) {
+                Some(ty2) => subtype(env, ty1, ty2),
+                None => false,
+            })
         }
         (Func(f1), Func(f2)) => {
             let m1: HashSet<_> = f1.modes.iter().collect();
@@ -80,46 +57,10 @@ pub fn subtype(env: &TypeEnv, t1: &Type, t2: &Type) -> bool {
             if m1 != m2 {
                 return false;
             }
-            let args1 = Record(
-                f1.args
-                    .iter()
-                    .enumerate()
-                    .map(|(i, ty)| Field {
-                        id: Label::Id(i as u32),
-                        ty: ty.clone(),
-                    })
-                    .collect(),
-            );
-            let args2 = Record(
-                f2.args
-                    .iter()
-                    .enumerate()
-                    .map(|(i, ty)| Field {
-                        id: Label::Id(i as u32),
-                        ty: ty.clone(),
-                    })
-                    .collect(),
-            );
-            let rets1 = Record(
-                f1.rets
-                    .iter()
-                    .enumerate()
-                    .map(|(i, ty)| Field {
-                        id: Label::Id(i as u32),
-                        ty: ty.clone(),
-                    })
-                    .collect(),
-            );
-            let rets2 = Record(
-                f2.rets
-                    .iter()
-                    .enumerate()
-                    .map(|(i, ty)| Field {
-                        id: Label::Id(i as u32),
-                        ty: ty.clone(),
-                    })
-                    .collect(),
-            );
+            let args1 = to_tuple(&f1.args);
+            let args2 = to_tuple(&f2.args);
+            let rets1 = to_tuple(&f1.rets);
+            let rets2 = to_tuple(&f2.rets);
             subtype(env, &args2, &args1) && subtype(env, &rets1, &rets2)
         }
         (Class(_, _), Class(_, _)) => unreachable!(),
@@ -127,4 +68,16 @@ pub fn subtype(env: &TypeEnv, t1: &Type, t2: &Type) -> bool {
         (_, Unknown) => unreachable!(),
         (_, _) => false,
     }
+}
+
+fn to_tuple(args: &[Type]) -> Type {
+    Type::Record(
+        args.iter()
+            .enumerate()
+            .map(|(i, ty)| Field {
+                id: Label::Id(i as u32),
+                ty: ty.clone(),
+            })
+            .collect(),
+    )
 }
