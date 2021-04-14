f51e5134d7c5440340aabe871083940cca46ffab
chenyan-dfinity <yan.chen@dfinity.org>
2021-04-13 17:42:50 -0700

    fix typescript binding for tuple

--- rust/candid/tests/assets/ok/fieldnat.js
+++ rust/candid/tests/assets/ok/fieldnat.js
@@ -1,4 +1,6 @@
 export default ({ IDL }) => {
+  const tuple = IDL.Tuple(IDL.Text, IDL.Text);
+  const non_tuple = IDL.Record({ _1_ : IDL.Text, _2_ : IDL.Text });
   return IDL.Service({
     'bab' : IDL.Func([IDL.Int, IDL.Nat], [], []),
     'bar' : IDL.Func([IDL.Record({ '2' : IDL.Int })], [], []),
@@ -12,6 +14,7 @@ export default ({ IDL }) => {
         [IDL.Record({})],
         [],
       ),
+    'bba' : IDL.Func([tuple], [non_tuple], []),
     'bib' : IDL.Func(
         [IDL.Tuple(IDL.Int)],
         [IDL.Variant({ _0_ : IDL.Int })],
