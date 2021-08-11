6dd017143838a76e1145f29593ecb0d751168052
Yan Chen <48968912+chenyan-dfinity@users.noreply.github.com>
2021-08-11 11:28:18 -0700

    add binding generation for Candid UI canister (#267)
    * add binding generation for Candid UI canister
    
    * update did
    
    * fix error style

--- tools/ui/src/didjs/lib.rs
+++ tools/ui/src/didjs/lib.rs
@@ -1,5 +1,6 @@
 use ic_cdk::export::candid::{
-    candid_method, check_prog, export_service, CandidType, Deserialize, IDLProg, TypeEnv,
+    candid_method, check_prog, export_service, types::subtype, types::Type, CandidType,
+    Deserialize, IDLProg, TypeEnv,
 };
 
 #[derive(CandidType, Deserialize)]
@@ -30,6 +31,43 @@ fn did_to_js(prog: String) -> Option<String> {
     let actor = check_prog(&mut env, &ast).ok()?;
     let res = ic_cdk::export::candid::bindings::javascript::compile(&env, &actor);
     Some(res)
+}
+
+#[ic_cdk_macros::query]
+#[candid_method(query)]
+fn binding(prog: String, lang: String) -> Option<String> {
+    use ic_cdk::export::candid::bindings;
+    let ast = prog.parse::<IDLProg>().ok()?;
+    let mut env = TypeEnv::new();
+    let actor = check_prog(&mut env, &ast).ok()?;
+    let res = match lang.as_str() {
+        "ts" => bindings::typescript::compile(&env, &actor),
+        "mo" => bindings::motoko::compile(&env, &actor),
+        "installed_did" => {
+            let actor = if let Some(Type::Class(_, ty)) = actor {
+                Some(*ty)
+            } else {
+                actor
+            };
+            bindings::candid::compile(&env, &actor)
+        }
+        _ => return None,
+    };
+    Some(res)
+}
+
+#[ic_cdk_macros::query]
+#[candid_method(query)]
+fn subtype(new: String, old: String) -> Result<(), String> {
+    let new = new.parse::<IDLProg>().unwrap();
+    let old = old.parse::<IDLProg>().unwrap();
+    let mut new_env = TypeEnv::new();
+    let mut old_env = TypeEnv::new();
+    let new_actor = check_prog(&mut new_env, &new).unwrap().unwrap();
+    let old_actor = check_prog(&mut old_env, &old).unwrap().unwrap();
+    let mut gamma = std::collections::HashSet::new();
+    subtype::subtype(&mut gamma, &new_env, &new_actor, &old_env, &old_actor)
+        .or_else(|e| Err(e.to_string()))
 }
 
 fn retrieve(path: &str) -> Option<&'static [u8]> {
