eca0d281a73401ecadcb9f00f847dfdf49ff83a1
Daniel Bloom <82895745+Daniel-Bloom-dfinity@users.noreply.github.com>
2022-02-11 12:03:56 -0800

    feat: mute `special opt FIXME` warning via feature (#317)

--- rust/candid/src/types/subtype.rs
+++ rust/candid/src/types/subtype.rs
@@ -42,6 +42,7 @@ pub fn subtype(gamma: &mut Gamma, env: &TypeEnv, t1: &Type, t2: &Type) -> Result
             Ok(())
         }
         (t1, Opt(_)) => {
+            #[cfg(not(feature = "mute_warnings"))]
             eprintln!("FIX ME! {} <: {} via special opt rule.\nThis means the sender and receiver type has diverged, and can cause data loss.", t1, t2);
             Ok(())
         }
