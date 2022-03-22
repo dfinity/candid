4a11c5849b0dc948c7390563f0c3cd793da68273
Daniel Bloom <82895745+Daniel-Bloom-dfinity@users.noreply.github.com>
2022-03-22 08:20:41 -0700

    feat: Expose `FuncVisitor` (#325)

--- rust/candid/src/types/reference.rs
+++ rust/candid/src/types/reference.rs
@@ -43,30 +43,33 @@ impl CandidType for Service {
     }
 }
 
+/// A [`Visitor`] to extract [`Func`]s.
+pub struct FuncVisitor;
+
+impl<'de> Visitor<'de> for FuncVisitor {
+    type Value = Func;
+    fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        formatter.write_str("Func value")
+    }
+    fn visit_byte_buf<E: de::Error>(self, bytes: Vec<u8>) -> Result<Func, E> {
+        if bytes[0] != 5u8 {
+            return Err(de::Error::custom("not func"));
+        }
+        let mut bytes = &bytes[1..];
+        let len = leb128::read::unsigned(&mut bytes).map_err(E::custom)? as usize;
+        let mut buf = vec![0; len];
+        bytes.read_exact(&mut buf).map_err(E::custom)?;
+        let method = String::from_utf8(buf).map_err(E::custom)?;
+        let principal = Principal::try_from(bytes).map_err(E::custom)?;
+        Ok(Func { principal, method })
+    }
+}
+
 impl<'de> Deserialize<'de> for Func {
     fn deserialize<D>(deserializer: D) -> Result<Func, D::Error>
     where
         D: serde::Deserializer<'de>,
     {
-        struct FuncVisitor;
-        impl<'de> Visitor<'de> for FuncVisitor {
-            type Value = Func;
-            fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
-                formatter.write_str("Func value")
-            }
-            fn visit_byte_buf<E: de::Error>(self, bytes: Vec<u8>) -> Result<Func, E> {
-                if bytes[0] != 5u8 {
-                    return Err(de::Error::custom("not func"));
-                }
-                let mut bytes = &bytes[1..];
-                let len = leb128::read::unsigned(&mut bytes).map_err(E::custom)? as usize;
-                let mut buf = vec![0; len];
-                bytes.read_exact(&mut buf).map_err(E::custom)?;
-                let method = String::from_utf8(buf).map_err(E::custom)?;
-                let principal = Principal::try_from(bytes).map_err(E::custom)?;
-                Ok(Func { principal, method })
-            }
-        }
         deserializer.deserialize_any(FuncVisitor)
     }
 }
