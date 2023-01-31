38906efe9aabe3dbdfd1b5700a30a8f33a025a10
Venkkatesh Sekar <venkkatesh.sekar@dfinity.org>
2023-01-31 14:18:15 +0000

    Update fuzzer

--- rust/candid/fuzz/fuzz_targets/parser.rs
+++ rust/candid/fuzz/fuzz_targets/parser.rs
@@ -2,10 +2,12 @@
 use libfuzzer_sys::fuzz_target;
 use std::slice;
 use candid::parser::{
-    types::IDLProg,
-    typing::{check_prog, TypeEnv},
-    value::{IDLArgs, IDLField, IDLValue},
+    types::IDLProg
 };
+use candid::types::{
+    value::{IDLArgs, IDLValue},
+};
+
 use candid::types::Label;
 use candid::{decode_args, decode_one, Decode};
 
