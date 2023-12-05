c12170c0cf50fc25f06f03fdf27479f64a3d4626
Yan Chen <yan.chen@dfinity.org>
2023-12-04 19:37:49 -0800

    refactor candid ui auth

--- tools/ui/src/auth/auth.ts
+++ tools/ui/src/auth/auth.ts
@@ -2,135 +2,124 @@ import { Principal } from "@dfinity/principal"
 import { authClient } from "../candid"
 import { refresh_actor } from "../index"
 import { dfinityLogo, copyIcon } from "./icons"
-import { identityProvider } from "./identityProvider"
 
 export async function renderAuth() {
-  ;(await authClient?.isAuthenticated()) ? insertLogout() : insertLoginForm()
+  const is_logged = await authClient?.isAuthenticated();
+  is_logged ? insertLogout() : insertLoginForm();
 }
 
-type DerivationOriginOptions = {
-  canisterId: Principal
-  domain: string
-  raw: boolean
+function is_valid_url(url: string): boolean {
+  let obj;
+  try {
+    obj = new URL(url);
+  } catch (_) {
+    return false;
+  }
+  return obj.protocol === "http:" || obj.protocol === "https:";
 }
 
 function insertLoginForm() {
-  const params = new URLSearchParams(window.location.search)
-  const cid = params.get("id")
-  if (!cid) {
-    throw new Error("No canister ID available for authentication")
-  } else {
-    const auth = document.getElementById("authentication")!
+  const auth = document.getElementById("authentication")!;
+  const buttonLogin = document.createElement("button");
+  buttonLogin.className = "btn btn-auth";
+  buttonLogin.innerHTML = `${dfinityLogo} Login`;
+  auth.appendChild(buttonLogin);
 
-    const buttonLogin = document.createElement("button")
-    buttonLogin.className = "btn btn-auth"
-    buttonLogin.innerHTML = `${dfinityLogo} Login`
-
-    const canisterId = Principal.fromText(cid)
+  try {
+    const params = new URLSearchParams(window.location.search);
+    const is_mainnet = window.location.hostname.endsWith("icp0.io") || window.location.hostname.endsWith("ic0.app");
+    let provider = params.get("ii");
+    if (is_mainnet && !provider) {
+      provider = "https://identity.internetcomputer.org";
+    }
+    if (!provider) {
+      throw new Error("Please provide internet identity url in ii parameter");
+    }
+    if (provider && !is_valid_url(provider)) {
+      throw new Error("Please provide a valid internet identity url in ii parameter");
+    }
+    const origin = params.get("origin");
+    if (origin && !is_valid_url(origin)) {
+      throw new Error("Please provide a valid derivationOrigin url in origin parameter");
+    }
+    const cid = Principal.fromText(params.get("id")!);
 
     buttonLogin.addEventListener("click", async () => {
-      const options: DerivationOriginOptions = {
-        canisterId,
-        domain: (document.getElementById("domain") as HTMLInputElement).checked
-          ? "icp0.io"
-          : "ic0.app",
-        raw: (document.getElementById("raw") as HTMLInputElement).checked,
+      let config: any = {
+        identityProvider: provider,
+        onSuccess: async () => {
+          refresh_actor(cid);
+          insertLogout();
+        },
+        onError: (err: any) => console.error(err),
+      };
+      if (origin) {
+        config = {...config, derivationOrigin: origin};
       }
-      await login(options)
-    })
-
-    auth.innerHTML = `
-      <label class="checkbox-label">
-        <input id="raw" type="checkbox" checked></input>
-        raw
-      </label>
-      <label class="checkbox-label">
-        <input id="domain" type="checkbox" checked></input>
-        ic0
-      </label>
-    `
-    auth.appendChild(buttonLogin);
+      await authClient?.login(config);
+    });
+  } catch (err) {
+    buttonLogin.disabled = true;
+    buttonLogin.title = (err as any).toString();
   }
 }
 
 function insertLogout() {
-  const auth = document.getElementById("authentication")
-  auth!.innerHTML = ""
+  const auth = document.getElementById("authentication");
+  auth!.innerHTML = "";
 
-  CopyId()
-  LogoutButton()
+  CopyId();
+  LogoutButton();
 }
 
 function LogoutButton() {
-  const auth = document.getElementById("authentication")
+  const auth = document.getElementById("authentication");
 
-  const buttonLogout = document.createElement("button")
-  buttonLogout.className = "btn random"
-  buttonLogout.style.margin = "10px"
-  buttonLogout.innerText = "Logout"
+  const buttonLogout = document.createElement("button");
+  buttonLogout.className = "btn random";
+  buttonLogout.style.margin = "10px";
+  buttonLogout.innerText = "Logout";
 
   buttonLogout.addEventListener("click", async () => {
-    await logout()
+    await logout();
   })
 
-  auth!.appendChild(buttonLogout)
+  auth!.appendChild(buttonLogout);
 }
 
 function CopyId() {
   if (!authClient) {
-    return
+    return;
   }
 
-  const auth = document.getElementById("authentication")
+  const auth = document.getElementById("authentication");
 
-  const copyText = document.createElement("span")
+  const copyText = document.createElement("span");
 
-  const id = authClient.getIdentity().getPrincipal().toString()
-  const idShort = id.slice(0, 5) + "..." + id.slice(-5)
-  copyText.innerText = idShort
+  const id = authClient.getIdentity().getPrincipal().toString();
+  const idShort = id.slice(0, 5) + "..." + id.slice(-5);
+  copyText.innerText = idShort;
 
-  const copyButton = document.createElement("button")
-  copyButton.id = "copyButton"
-  copyButton.style.cursor = "pointer"
-  copyButton.innerHTML = `${copyIcon}`
+  const copyButton = document.createElement("button");
+  copyButton.id = "copyButton";
+  copyButton.style.cursor = "pointer";
+  copyButton.innerHTML = `${copyIcon}`;
 
   copyButton.addEventListener("click", function () {
     navigator.clipboard.writeText(id).catch((err) => {
-      console.error(err)
+      console.error(err);
     })
-    copyText.innerText = "Copied!"
+    copyText.innerText = "Copied!";
     setTimeout(() => {
-      copyText.innerText = idShort
+      copyText.innerText = idShort;
     }, 2000)
   })
 
-  auth?.appendChild(copyButton)
-  auth?.appendChild(copyText)
-}
-
-async function login(options: DerivationOriginOptions) {
-  authClient?.login({
-    identityProvider,
-    derivationOrigin: derivationOrigin(options),
-    onSuccess: async () => {
-      refresh_actor(options.canisterId)
-      insertLogout()
-    },
-    onError: (err) => console.error(err),
-  })
+  auth?.appendChild(copyButton);
+  auth?.appendChild(copyText);
 }
 
 async function logout() {
-  authClient?.logout()
-  window.location.reload()
-}
-
-function derivationOrigin(options: DerivationOriginOptions): string {
-  if (options.domain !== "icp0.io" && options.domain !== "ic0.app") {
-    throw new Error("Invalid domain")
-  }
-
-  return options.raw
-    ? "https://" + options.canisterId.toString() + ".raw." + options.domain
-    : "https://" + options.canisterId.toString() + "." + options.domain
+  authClient?.logout();
+  window.location.reload();
 }
