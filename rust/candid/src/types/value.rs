16c167a10484824f19696d9c556aff4887aa5e5c
Max <bitdivine@users.noreply.github.com>
2023-12-07 04:08:21 +0100

    feat: Make it easy to convert data to IDLValue and candid text format (#502)
    # Changes
    - Add a method to convert Rust data to IDLValue and hence text-format Candid: `try_from_candid_type()`
    
    ---------
    
    Co-authored-by: Yan Chen <yan.chen@dfinity.org>
    Co-authored-by: Yan Chen <48968912+chenyan-dfinity@users.noreply.github.com>

--- rust/candid/src/types/value.rs
+++ rust/candid/src/types/value.rs
@@ -1,6 +1,6 @@
 use crate::types::number::{Int, Nat};
 use crate::types::{Field, Label, Type, TypeEnv, TypeInner};
-use crate::{Error, Result};
+use crate::{CandidType, Error, Result};
 use serde::de;
 use serde::de::{Deserialize, Visitor};
 use std::collections::HashMap;
@@ -334,6 +334,32 @@ impl IDLValue {
             }
         }
         .into()
+    }
+
+    /// Converts data that implements [`CandidType`] into an [`IDLValue`].
+    ///
+    /// # Example: Convert data to candid text format
+    /// ```
+    /// # use candid::{CandidType, IDLValue};
+    /// #[derive(CandidType)]
+    /// struct MyStruct {
+    ///    a: u8,
+    ///   b: String,
+    /// }
+    /// let my_struct = MyStruct { a: 42, b: "hello".to_string() };
+    /// let idl_value = IDLValue::try_from_candid_type(&my_struct).unwrap();
+    /// let expected_text = "record { a = 42 : nat8; b = \"hello\" }";
+    /// let actual_text = idl_value.to_string();
+    /// assert_eq!(expected_text, actual_text);
+    /// ```
+    pub fn try_from_candid_type<T>(data: &T) -> Result<Self>
+    where
+        T: CandidType,
+    {
+        use crate::Encode;
+        let blob = Encode!(data)?;
+        let args = IDLArgs::from_bytes_with_types(&blob, &TypeEnv::default(), &[T::ty()])?;
+        Ok(args.args[0].clone())
     }
 }
 
