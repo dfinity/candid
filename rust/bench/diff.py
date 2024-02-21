0b16febcf3ed057436d560ccaf62bafddff977cc
Yan Chen <48968912+chenyan-dfinity@users.noreply.github.com>
2024-02-21 10:09:49 -0800

    Bump agent-js (#529)
    * bump agent-js in Candid UI
    
    * add diff

--- rust/bench/diff.py
+++ rust/bench/diff.py
@@ -1,25 +1,63 @@
+import sys
 import yaml
 
 special_benchmarks = ["extra_args"]
+
+if (len(sys.argv) < 3):
+  print("Usage: python diff.py <current.yml> <base.yml>")
+  sys.exit(1)
 
 def load_yaml(input_file):
   # Load the YAML data
   with open(input_file, 'r') as f:
     data = yaml.safe_load(f)["benches"]
+  return data
 
-  print("| Name | Max Mem(Kb) | Encode | Decode |")
-  print("| --- | --- | --- | --- |")
+def get_numbers(name, data):
+  scope = data[name]["scopes"]
+  mem = data[name]["total"]["heap_increase"] * 64
+  encode = scope["1. Encoding"]["instructions"]
+  decode = scope["2. Decoding"]["instructions"]
+  return (mem, encode, decode)
+
+def display_diff(current, base):
+  if base == 0:
+    d = 0
+  else:
+    d = (current - base) / base * 100
+  if d < 0:
+    return f"{current:_} ($\\textcolor{{green}}{{{d:.2f}\\\\%}}$)"
+  elif d > 0:
+    return f"{current:_} ($\\textcolor{{red}}{{{d:.2f}\\\\%}}$)"
+  else:
+    return f"{current:_}"
+
+def print_table(data, base):
+  print("| Name | Max Mem (Kb) | Encode | Decode |")
+  print("|:--- |:--- |:--- |:---|")
   for name in data.keys():
     if name not in special_benchmarks:
-      scope = data[name]["scopes"]
-      mem = data[name]["total"]["heap_increase"] * 64
-      encode = scope["1. Encoding"]["instructions"]
-      decode = scope["2. Decoding"]["instructions"]
-      print(f"| {name} | {mem:_} | {encode:_} | {decode:_} |")
+      mem, encode, decode = get_numbers(name, data)
+      if base.get(name) is not None:
+        base_mem, base_encode, base_decode = get_numbers(name, base)
+        mem = display_diff(mem, base_mem)
+        encode = display_diff(encode, base_encode)
+        decode = display_diff(decode, base_decode)
+      else:
+        name = "**" + name + " (new)**"
+        mem = f"{mem:_}"
+        encode = f"{encode:_}"
+        decode = f"{decode:_}"
+      print(f"| {name} | {mem} | {encode} | {decode} |")
 
   print()
-  print(f"* Parser cost: {data['nns']['scopes']['0. Parsing']['instructions']:_}")
-  print(f"* Extra args: {data['extra_args']['total']['instructions']:_}")
+  parse = data["nns"]["scopes"]["0. Parsing"]["instructions"]
+  base_parse = base["nns"]["scopes"]["0. Parsing"]["instructions"]
+  print(f"* Parser cost: {display_diff(parse, base_parse)}")
+  extra = data["extra_args"]["total"]["instructions"]
+  base_extra = base["extra_args"]["total"]["instructions"]
+  print(f"* Extra args: {display_diff(extra, base_extra)}")
 
-input_file = "canbench_results.yml"
-load_yaml(input_file)
+current = load_yaml(sys.argv[1])
+base = load_yaml(sys.argv[2])
+print_table(current, base)
