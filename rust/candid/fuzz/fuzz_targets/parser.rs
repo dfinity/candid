6559b0d8987920efa6eeb383130c5d977e88495c
Yan Chen <48968912+chenyan-dfinity@users.noreply.github.com>
2024-03-08 13:22:20 -0800

    avoid cost overflow and fix principal cost (#534)
    * avoid cost overflow and fix principal cost
    
    * update fuzzer
    
    * remove bench_result
    
    * bump version

--- rust/candid/fuzz/fuzz_targets/parser.rs
+++ rust/candid/fuzz/fuzz_targets/parser.rs
@@ -3,7 +3,9 @@ use candid::types::value::IDLArgs;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    let decoded = match IDLArgs::from_bytes(&data) {
+    let mut config = candid::DecoderConfig::new();
+    config.set_decoding_quota(2_000_000_000); // already scaled by 50x due to untyped
+    let decoded = match IDLArgs::from_bytes_with_config(&data, &config) {
         Ok(_v) => _v,
         Err(_e) => return,
     };
