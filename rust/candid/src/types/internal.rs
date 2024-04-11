ae4d0f794a808b88f8b83da7eb96ca87b3d751d4
Yan Chen <48968912+chenyan-dfinity@users.noreply.github.com>
2024-04-11 09:59:33 -0700

    use btreemap for type_map and release (#542)
    * use btreemap for type_map
    
    * more btreemap
    
    * fix
    
    * disable unroll
    
    * bump and changelog

--- rust/candid/src/types/internal.rs
+++ rust/candid/src/types/internal.rs
@@ -1,13 +1,13 @@
 use super::CandidType;
 use crate::idl_hash;
 use std::cell::RefCell;
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::fmt;
 
 // This is a re-implementation of std::any::TypeId to get rid of 'static constraint.
 // The current TypeId doesn't consider lifetime while computing the hash, which is
 // totally fine for Candid type, as we don't care about lifetime at all.
-#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone, PartialOrd, Ord)]
 pub struct TypeId {
     id: usize,
     pub name: &'static str,
@@ -33,8 +33,8 @@ pub fn type_of<T>(_: &T) -> TypeId {
 
 #[derive(Default)]
 struct TypeName {
-    type_name: HashMap<TypeId, String>,
-    name_index: HashMap<String, usize>,
+    type_name: BTreeMap<TypeId, String>,
+    name_index: BTreeMap<String, usize>,
 }
 impl TypeName {
     fn get(&mut self, id: &TypeId) -> String {
@@ -164,10 +164,10 @@ impl TypeContainer {
     }
 }
 
-#[derive(Debug, PartialEq, Hash, Eq, Clone)]
+#[derive(Debug, PartialEq, Hash, Eq, Clone, PartialOrd, Ord)]
 pub struct Type(pub std::rc::Rc<TypeInner>);
 
-#[derive(Debug, PartialEq, Hash, Eq, Clone)]
+#[derive(Debug, PartialEq, Hash, Eq, Clone, PartialOrd, Ord)]
 pub enum TypeInner {
     Null,
     Bool,
@@ -382,7 +382,7 @@ pub fn text_size(t: &Type, limit: i32) -> Result<i32, ()> {
     }
 }
 
-#[derive(Debug, Eq, Clone)]
+#[derive(Debug, Eq, Clone, PartialOrd, Ord)]
 pub enum Label {
     Id(u32),
     Named(String),
@@ -423,7 +423,7 @@ impl std::hash::Hash for Label {
 
 pub type SharedLabel = std::rc::Rc<Label>;
 
-#[derive(Debug, PartialEq, Hash, Eq, Clone)]
+#[derive(Debug, PartialEq, Hash, Eq, Clone, PartialOrd, Ord)]
 pub struct Field {
     pub id: SharedLabel,
     pub ty: Type,
@@ -486,13 +486,13 @@ macro_rules! variant {
     }}
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub enum FuncMode {
     Oneway,
     Query,
     CompositeQuery,
 }
-#[derive(Debug, PartialEq, Hash, Eq, Clone)]
+#[derive(Debug, PartialEq, Hash, Eq, Clone, PartialOrd, Ord)]
 pub struct Function {
     pub modes: Vec<FuncMode>,
     pub args: Vec<Type>,
@@ -626,9 +626,9 @@ pub fn unroll(t: &Type) -> Type {
 }
 
 thread_local! {
-    static ENV: RefCell<HashMap<TypeId, Type>> = RefCell::new(HashMap::new());
+    static ENV: RefCell<BTreeMap<TypeId, Type>> = const { RefCell::new(BTreeMap::new()) };
     // only used for TypeContainer
-    static ID: RefCell<HashMap<Type, TypeId>> = RefCell::new(HashMap::new());
+    static ID: RefCell<BTreeMap<Type, TypeId>> = const { RefCell::new(BTreeMap::new()) };
     static NAME: RefCell<TypeName> = RefCell::new(TypeName::default());
 }
 
