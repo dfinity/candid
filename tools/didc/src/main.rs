de346526a93ca80226a27f21880ea62c4ab3df6b
Jordan Last <jordan.michael.last@gmail.com>
2024-09-19 13:11:35 -0600

    implemented exporting of IDL objects along with ts-js target for combined TypeScript file with the JavaScript runtime values

--- tools/didc/src/main.rs
+++ tools/didc/src/main.rs
@@ -31,7 +31,7 @@ enum Command {
     Bind {
         /// Specifies did file for code generation
         input: PathBuf,
-        #[clap(short, long, value_parser = ["js", "ts", "did", "mo", "rs", "rs-agent", "rs-stub"])]
+        #[clap(short, long, value_parser = ["js", "ts", "ts-js", "did", "mo", "rs", "rs-agent", "rs-stub"])]
         /// Specifies target language
         target: String,
         #[clap(short, long)]
@@ -230,8 +230,11 @@ fn main() -> Result<()> {
                 )?);
             }
             let content = match target.as_str() {
-                "js" => candid_parser::bindings::javascript::compile(&env, &actor),
-                "ts" => candid_parser::bindings::typescript::compile(&env, &actor),
+                "js" => candid_parser::bindings::javascript::compile(&env, &actor, false),
+                "ts" => candid_parser::bindings::typescript::compile(&env, &actor, false),
+                "ts-js" => {
+                    candid_parser::bindings::typescript_and_javascript::compile(&env, &actor)
+                }
                 "did" => candid_parser::pretty::candid::compile(&env, &actor),
                 "mo" => candid_parser::bindings::motoko::compile(&env, &actor),
                 "rs" => {
