59261919d9748c042cd8562463e9565dd0e4947c
ilbertt <luca.bertelli@dfinity.org>
2025-06-10 10:35:33 +0200

    fix: count for " : " only if there's an arg name

--- rust/candid/src/types/internal.rs
+++ rust/candid/src/types/internal.rs
@@ -382,8 +382,15 @@ pub fn text_size(t: &Type, limit: i32) -> Result<i32, ()> {
             let mut cnt = mode + 6;
             let mut limit = limit - cnt;
             for t in &func.args {
-                let id_size = t.name.as_ref().map(|n| n.len() as i32).unwrap_or(0);
-                cnt += id_size + text_size(&t.typ, limit - id_size - 3)? + 3;
+                match t.name.as_ref() {
+                    Some(name) => {
+                        let id_size = name.len() as i32;
+                        cnt += id_size + text_size(&t.typ, limit - id_size - 3)? + 3;
+                    }
+                    None => {
+                        cnt += text_size(&t.typ, limit)?;
+                    }
+                }
                 limit -= cnt;
             }
             for t in &func.rets {
