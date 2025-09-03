e0b265deff6ab600e41289abf2cbe020604034e0
Linwei Shang <linwei.shang@dfinity.org>
2025-09-03 15:49:07 -0400

    feat: impl serde::Serialize for Reserved

--- rust/candid/src/types/reserved.rs
+++ rust/candid/src/types/reserved.rs
@@ -2,9 +2,10 @@
 
 use super::{CandidType, Serializer, Type, TypeInner};
 use serde::de::{self, Deserialize, Deserializer, Visitor};
+use serde::Serialize;
 use std::fmt;
 
-#[derive(Clone, Copy, Debug, Default, Eq, PartialEq)]
+#[derive(Clone, Copy, Debug, Default, Eq, PartialEq, Serialize)]
 pub struct Reserved;
 #[derive(PartialEq, Eq, Debug)]
 pub enum Empty {}
@@ -57,5 +58,24 @@ impl<'de> Deserialize<'de> for Empty {
             }
         }
         deserializer.deserialize_any(EmptyVisitor)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde::Deserialize;
+
+    #[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq)]
+    pub struct TestStruct {
+        inner: Reserved,
+    }
+
+    #[test]
+    fn test_serde_with_json() {
+        let test_struct = TestStruct { inner: Reserved };
+        let serialized = serde_json::to_string(&test_struct).unwrap();
+        let deserialized = serde_json::from_str(&serialized).unwrap();
+        assert_eq!(test_struct, deserialized);
     }
 }
