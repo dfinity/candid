d7ec5a9ddde96b918108092cb81da4c413d2c813
Adam Spofford <adam.spofford@dfinity.org>
2025-09-03 13:59:16 -0700

    Use manual null instead of derive for unit struct

--- rust/candid/src/types/reserved.rs
+++ rust/candid/src/types/reserved.rs
@@ -5,7 +5,7 @@ use serde::de::{self, Deserialize, Deserializer, Visitor};
 use serde::Serialize;
 use std::fmt;
 
-#[derive(Clone, Copy, Debug, Default, Eq, PartialEq, PartialOrd, Ord, Hash, Serialize)]
+#[derive(Clone, Copy, Debug, Default, Eq, PartialEq, PartialOrd, Ord, Hash)]
 pub struct Reserved;
 #[derive(PartialEq, Eq, Debug)]
 pub enum Empty {}
@@ -31,6 +31,15 @@ impl CandidType for Empty {
         S: Serializer,
     {
         serializer.serialize_empty()
+    }
+}
+
+impl Serialize for Reserved {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_none()
     }
 }
 
@@ -71,11 +80,17 @@ mod tests {
         inner: Reserved,
     }
 
+    #[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq)]
+    pub struct TestStructUpgraded {
+        inner: Option<String>,
+    }
+
     #[test]
     fn test_serde_with_json() {
         let test_struct = TestStruct { inner: Reserved };
         let serialized = serde_json::to_string(&test_struct).unwrap();
         let deserialized = serde_json::from_str(&serialized).unwrap();
         assert_eq!(test_struct, deserialized);
+        let _upgraded = serde_json::from_str::<TestStructUpgraded>(&serialized).unwrap();
     }
 }
