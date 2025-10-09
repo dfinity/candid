dfinity/candid

commit 61a49c8763824916b84bbafe7a5d63952d5e001d

feat: support argument names (#612)
**Overview**
Adds support for named arguments: if a `.did` Candid declaration file
declares functions, classes or services with named arguments, those
names are parsed by the `candid-parser` and preserved in the generated
bindings.
At the same time, the names of the arguments are preserved if the Candid
declaration file is generated from a Rust canister.

**Requirements**
To consider this feature complete, the Motoko compiler should preserve
the actor methods names from Motoko canisters to the Candid declaration
files.

**Considerations**
This change should be backwards compatible.

Picked up from https://github.com/ilbertt/candid/tree/luca/arg-names

rust/candid/src/binary_parser.rs
@@ -1,4 +1,4 @@
-use crate::types::internal::{Field, Function, Label, Type, TypeInner};
+use crate::types::internal::{ArgType, Field, Function, Label, Type, TypeInner};
 use crate::types::{FuncMode, TypeEnv};
 use anyhow::{anyhow, Context, Result};
 use binread::io::{Read, Seek};
@@ -201,7 +201,10 @@ impl ConsType {
                 let mut args = Vec::new();
                 let mut rets = Vec::new();
                 for arg in &f.args {
-                    args.push(arg.to_type(len)?);
+                    args.push(ArgType {
+                        name: None,
+                        typ: arg.to_type(len)?,
+                    });
                 }
                 for ret in &f.rets {
                     rets.push(ret.to_type(len)?);

rust/candid/src/pretty/candid.rs
@@ -1,5 +1,7 @@
 use crate::pretty::utils::*;
-use crate::types::{Field, FuncMode, Function, Label, SharedLabel, Type, TypeEnv, TypeInner};
+use crate::types::{
+    ArgType, Field, FuncMode, Function, Label, SharedLabel, Type, TypeEnv, TypeInner,
+};
 use pretty::RcDoc;
 
 static KEYWORDS: [&str; 30] = [
@@ -149,7 +151,7 @@ fn pp_fields(fs: &[Field], is_variant: bool) -> RcDoc {
 
 pub fn pp_function(func: &Function) -> RcDoc {
     let args = pp_args(&func.args);
-    let rets = pp_args(&func.rets);
+    let rets = pp_rets(&func.rets);
     let modes = pp_modes(&func.modes);
     args.append(" ->")
         .append(RcDoc::space())
@@ -157,10 +159,23 @@ pub fn pp_function(func: &Function) -> RcDoc {
         .nest(INDENT_SPACE)
 }
 
-pub fn pp_args(args: &[Type]) -> RcDoc {
-    let doc = concat(args.iter().map(pp_ty), ",");
+pub fn pp_args(args: &[ArgType]) -> RcDoc {
+    let args = args.iter().map(|arg| {
+        if let Some(name) = &arg.name {
+            pp_text(name).append(kwd(" :")).append(pp_ty(&arg.typ))
+        } else {
+            pp_ty(&arg.typ)
+        }
+    });
+    let doc = concat(args, ",");
     enclose("(", doc, ")")
 }
+
+pub fn pp_rets(rets: &[Type]) -> RcDoc {
+    let doc = concat(rets.iter().map(pp_ty), ",");
+    enclose("(", doc, ")")
+}
+
 pub fn pp_mode(mode: &FuncMode) -> RcDoc {
     match mode {
         FuncMode::Oneway => RcDoc::text("oneway"),
@@ -205,7 +220,7 @@ fn pp_actor(ty: &Type) -> RcDoc {
     }
 }
 
-pub fn pp_init_args<'a>(env: &'a TypeEnv, args: &'a [Type]) -> RcDoc<'a> {
+pub fn pp_init_args<'a>(env: &'a TypeEnv, args: &'a [ArgType]) -> RcDoc<'a> {
     pp_defs(env).append(pp_args(args))
 }
 pub fn compile(env: &TypeEnv, actor: &Option<Type>) -> String {

commit 77731176bed02159c0aeee8c5cfe95ca1d051749

feat!: collect names for return types in parser (#684)
**Overview**
Named arguments can also be in the return type according to the spec.
This PR updates the grammar to collect the names of the return types.

As a consequence, it fixes a regression introduced in
https://github.com/dfinity/candid/pull/612/files#diff-c5ff9610c5d6538ec79f3d465965d2b3a3e3b628b16854a91b4b4a90ba766a57L220-L230,
in order to keep parsing the return types with arguments without errors.

Updates a test case to check if the behavior has been restored.

rust/candid/src/binary_parser.rs
@@ -205,7 +205,10 @@ impl ConsType {
                     });
                 }
                 for ret in &f.rets {
-                    rets.push(ret.to_type(len)?);
+                    rets.push(ArgType {
+                        name: None,
+                        typ: ret.to_type(len)?,
+                    });
                 }
                 TypeInner::Func(Function {
                     modes: f.ann.iter().map(|x| x.inner.clone()).collect(),

rust/candid/src/pretty/candid.rs
@@ -156,7 +156,7 @@ fn pp_fields(fs: &[Field], is_variant: bool) -> RcDoc<'_> {
 
 pub fn pp_function(func: &Function) -> RcDoc<'_> {
     let args = pp_named_args(&func.args);
-    let rets = pp_rets(&func.rets);
+    let rets = pp_named_args(&func.rets);
     let modes = pp_modes(&func.modes);
     args.append(" ->")
         .append(RcDoc::space())
@@ -183,11 +183,6 @@ pub fn pp_named_args(args: &[ArgType]) -> RcDoc<'_> {
 /// To print named arguments, use [`pp_named_args`] instead.
 pub fn pp_args(args: &[Type]) -> RcDoc<'_> {
     sep_enclose(args.iter().map(pp_ty), ",", "(", ")")
-}
-
-/// Pretty-prints return types in the form of `(type1, type2)`.
-pub fn pp_rets(args: &[Type]) -> RcDoc<'_> {
-    pp_args(args)
 }
 
 pub fn pp_mode(mode: &FuncMode) -> RcDoc<'_> {

rust/candid/src/ser.rs
@@ -339,7 +339,7 @@ impl TypeSerialize {
                     self.build_type(&ty.typ)?;
                 }
                 for ty in &func.rets {
-                    self.build_type(ty)?;
+                    self.build_type(&ty.typ)?;
                 }
                 sleb128_encode(&mut buf, Opcode::Func as i64)?;
                 leb128_encode(&mut buf, func.args.len() as u64)?;
@@ -348,7 +348,7 @@ impl TypeSerialize {
                 }
                 leb128_encode(&mut buf, func.rets.len() as u64)?;
                 for ty in &func.rets {
-                    self.encode(&mut buf, ty)?;
+                    self.encode(&mut buf, &ty.typ)?;
                 }
                 leb128_encode(&mut buf, func.modes.len() as u64)?;
                 for m in &func.modes {

rust/candid/src/types/internal.rs
@@ -163,7 +163,14 @@ impl TypeContainer {
                         typ: self.go(&arg.typ),
                     })
                     .collect(),
-                rets: func.rets.iter().map(|arg| self.go(arg)).collect(),
+                rets: func
+                    .rets
+                    .iter()
+                    .map(|arg| ArgType {
+                        name: arg.name.clone(),
+                        typ: self.go(&arg.typ),
+                    })
+                    .collect(),
             }),
             TypeInner::Service(serv) => TypeInner::Service(
                 serv.iter()
@@ -307,7 +314,14 @@ impl Type {
                             typ: t.typ.subst(tau),
                         })
                         .collect(),
-                    rets: func.rets.into_iter().map(|t| t.subst(tau)).collect(),
+                    rets: func
+                        .rets
+                        .into_iter()
+                        .map(|t| ArgType {
+                            name: t.name,
+                            typ: t.typ.subst(tau),
+                        })
+                        .collect(),
                 })
             }
             Service(serv) => Service(
@@ -398,7 +412,7 @@ pub fn text_size(t: &Type, limit: i32) -> Result<i32, ()> {
                 limit -= cnt;
             }
             for t in &func.rets {
-                cnt += text_size(t, limit)?;
+                cnt += text_size(&t.typ, limit)?;
                 limit -= cnt;
             }
             cnt
@@ -552,7 +566,7 @@ pub enum FuncMode {
 pub struct Function {
     pub modes: Vec<FuncMode>,
     pub args: Vec<ArgType>,
-    pub rets: Vec<Type>,
+    pub rets: Vec<ArgType>,
 }
 
 #[derive(Debug, PartialEq, Hash, Eq, Clone, PartialOrd, Ord)]
@@ -587,16 +601,16 @@ impl Function {
 /// `func!((u8, &str) -> (Nat) query)` expands to `Type(Rc::new(TypeInner::Func(...)))`
 macro_rules! func {
     ( ( $($arg:ty),* $(,)? ) -> ( $($ret:ty),* $(,)? ) ) => {
-        Into::<$crate::types::Type>::into($crate::types::TypeInner::Func($crate::types::Function { args: vec![$(<$arg>::ty()),*].into_iter().map(|arg| $crate::types::ArgType { name: None, typ: arg }).collect(), rets: vec![$(<$ret>::ty()),*], modes: vec![] }))
+        Into::<$crate::types::Type>::into($crate::types::TypeInner::Func($crate::types::Function { args: vec![$(<$arg>::ty()),*].into_iter().map(|arg| $crate::types::ArgType { name: None, typ: arg }).collect(), rets: vec![$(<$ret>::ty()),*].into_iter().map(|ret| $crate::types::ArgType { name: None, typ: ret }).collect(), modes: vec![] }))
     };
     ( ( $($arg:ty),* $(,)? ) -> ( $($ret:ty),* $(,)? ) query ) => {
-        Into::<$crate::types::Type>::into($crate::types::TypeInner::Func($crate::types::Function { args: vec![$(<$arg>::ty()),*].into_iter().map(|arg| $crate::types::ArgType { name: None, typ: arg }).collect(), rets: vec![$(<$ret>::ty()),*], modes: vec![$crate::types::FuncMode::Query] }))
+        Into::<$crate::types::Type>::into($crate::types::TypeInner::Func($crate::types::Function { args: vec![$(<$arg>::ty()),*].into_iter().map(|arg| $crate::types::ArgType { name: None, typ: arg }).collect(), rets: vec![$(<$ret>::ty()),*].into_iter().map(|ret| $crate::types::ArgType { name: None, typ: ret }).collect(), modes: vec![$crate::types::FuncMode::Query] }))
     };
     ( ( $($arg:ty),* $(,)? ) -> ( $($ret:ty),* $(,)? ) composite_query ) => {
-        Into::<$crate::types::Type>::into($crate::types::TypeInner::Func($crate::types::Function { args: vec![$(<$arg>::ty()),*].into_iter().map(|arg| $crate::types::ArgType { name: None, typ: arg }).collect(), rets: vec![$(<$ret>::ty()),*], modes: vec![$crate::types::FuncMode::CompositeQuery] }))
+        Into::<$crate::types::Type>::into($crate::types::TypeInner::Func($crate::types::Function { args: vec![$(<$arg>::ty()),*].into_iter().map(|arg| $crate::types::ArgType { name: None, typ: arg }).collect(), rets: vec![$(<$ret>::ty()),*].into_iter().map(|ret| $crate::types::ArgType { name: None, typ: ret }).collect(), modes: vec![$crate::types::FuncMode::CompositeQuery] }))
     };
     ( ( $($arg:ty),* $(,)? ) -> ( $($ret:ty),* $(,)? ) oneway ) => {
-        Into::<$crate::types::Type>::into($crate::types::TypeInner::Func($crate::types::Function { args: vec![$(<$arg>::ty()),*].into_iter().map(|arg| $crate::types::ArgType { name: None, typ: arg }).collect(), rets: vec![$(<$ret>::ty()),*], modes: vec![$crate::types::FuncMode::Oneway] }))
+        Into::<$crate::types::Type>::into($crate::types::TypeInner::Func($crate::types::Function { args: vec![$(<$arg>::ty()),*].into_iter().map(|arg| $crate::types::ArgType { name: None, typ: arg }).collect(), rets: vec![$(<$ret>::ty()),*].into_iter().map(|ret| $crate::types::ArgType { name: None, typ: ret }).collect(), modes: vec![$crate::types::FuncMode::Oneway] }))
     };
 }
 #[macro_export]

rust/candid/src/types/subtype.rs
@@ -1,5 +1,5 @@
 use super::internal::{find_type, Field, Label, Type, TypeInner};
-use crate::types::TypeEnv;
+use crate::types::{ArgType, TypeEnv};
 use crate::{Error, Result};
 use anyhow::Context;
 use std::collections::{HashMap, HashSet};
@@ -129,18 +129,8 @@ fn subtype_(
             if f1.modes != f2.modes {
                 return Err(Error::msg("Function mode mismatch"));
             }
-            let f1_args = f1
-                .args
-                .iter()
-                .map(|arg| arg.typ.clone())
-                .collect::<std::vec::Vec<_>>();
-            let f2_args = f2
-                .args
-                .iter()
-                .map(|arg| arg.typ.clone())
-                .collect::<std::vec::Vec<_>>();
-            let args1 = to_tuple(&f1_args);
-            let args2 = to_tuple(&f2_args);
+            let args1 = to_tuple(&f1.args);
+            let args2 = to_tuple(&f2.args);
             let rets1 = to_tuple(&f1.rets);
             let rets2 = to_tuple(&f2.rets);
             subtype_(report, gamma, env, &args2, &args1)
@@ -222,18 +212,8 @@ pub fn equal(gamma: &mut Gamma, env: &TypeEnv, t1: &Type, t2: &Type) -> Result<(
             if f1.modes != f2.modes {
                 return Err(Error::msg("Function mode mismatch"));
             }
-            let f1_args = f1
-                .args
-                .iter()
-                .map(|arg| arg.typ.clone())
-                .collect::<std::vec::Vec<_>>();
-            let f2_args = f2
-                .args
-                .iter()
-                .map(|arg| arg.typ.clone())
-                .collect::<std::vec::Vec<_>>();
-            let args1 = to_tuple(&f1_args);
-            let args2 = to_tuple(&f2_args);
+            let args1 = to_tuple(&f1.args);
+            let args2 = to_tuple(&f2.args);
             let rets1 = to_tuple(&f1.rets);
             let rets2 = to_tuple(&f2.rets);
             equal(gamma, env, &args1, &args2).context("Mismatch in function input type")?;
@@ -241,16 +221,8 @@ pub fn equal(gamma: &mut Gamma, env: &TypeEnv, t1: &Type, t2: &Type) -> Result<(
             Ok(())
         }
         (Class(init1, ty1), Class(init2, ty2)) => {
-            let init1_typ = init1
-                .iter()
-                .map(|arg| arg.typ.clone())
-                .collect::<std::vec::Vec<_>>();
-            let init2_typ = init2
-                .iter()
-                .map(|arg| arg.typ.clone())
-                .collect::<std::vec::Vec<_>>();
-            let init_1 = to_tuple(&init1_typ);
-            let init_2 = to_tuple(&init2_typ);
+            let init_1 = to_tuple(init1);
+            let init_2 = to_tuple(init2);
             equal(gamma, env, &init_1, &init_2).context(format!(
                 "Mismatch in init args: {} and {}",
                 pp_args(init1),
@@ -292,13 +264,13 @@ where
     }
 }
 
-fn to_tuple(args: &[Type]) -> Type {
+fn to_tuple(args: &[ArgType]) -> Type {
     TypeInner::Record(
         args.iter()
             .enumerate()
-            .map(|(i, ty)| Field {
+            .map(|(i, arg)| Field {
                 id: Label::Id(i as u32).into(),
-                ty: ty.clone(),
+                ty: arg.typ.clone(),
             })
             .collect(),
     )

rust/candid_derive/src/func.rs
@@ -140,7 +140,7 @@ pub(crate) fn export_service(path: Option<TokenStream>) -> TokenStream {
                         #doc_storage
                         let mut args: Vec<ArgType> = Vec::new();
                         #(#args)*
-                        let mut rets: Vec<Type> = Vec::new();
+                        let mut rets: Vec<ArgType> = Vec::new();
                         #(#rets)*
                         let func = Function { args, rets, modes: #modes };
                         service.push((#name.to_string(), TypeInner::Func(func).into()));
@@ -195,7 +195,7 @@ fn generate_arg(name: TokenStream, (arg_name, ty): &(Option<String>, String)) ->
 fn generate_ret(name: TokenStream, ty: &str) -> TokenStream {
     let ty = syn::parse_str::<Type>(ty).unwrap();
     quote! {
-        #name.push(env.add::<#ty>());
+        #name.push(ArgType { name: None, typ: env.add::<#ty>() });
     }
 }
 

rust/candid_parser/src/bindings/analysis.rs
@@ -58,7 +58,8 @@ pub fn chase_type<'a>(
         }
         Func(f) => {
             let args = f.args.iter().map(|arg| &arg.typ);
-            for ty in args.clone().chain(f.rets.iter()) {
+            let rets = f.rets.iter().map(|ret| &ret.typ);
+            for ty in args.chain(rets) {
                 chase_type(seen, res, env, ty)?;
             }
         }
@@ -117,7 +118,7 @@ pub fn chase_def_use<'a>(
         }
         for (i, arg) in func.rets.iter().enumerate() {
             let mut used = Vec::new();
-            chase_type(&mut BTreeSet::new(), &mut used, env, arg)?;
+            chase_type(&mut BTreeSet::new(), &mut used, env, &arg.typ)?;
             for var in used {
                 res.entry(var.to_string())
                     .or_insert_with(Vec::new)
@@ -162,7 +163,8 @@ pub fn infer_rec<'a>(_env: &'a TypeEnv, def_list: &'a [&'a str]) -> Result<BTree
             }
             Func(f) => {
                 let args = f.args.iter().map(|arg| &arg.typ);
-                for ty in args.clone().chain(f.rets.iter()) {
+                let rets = f.rets.iter().map(|ret| &ret.typ);
+                for ty in args.chain(rets) {
                     go(seen, res, _env, ty)?;
                 }
             }

rust/candid_parser/src/bindings/javascript.rs
@@ -161,18 +161,20 @@ fn pp_fields(fs: &[Field]) -> RcDoc<'_> {
 
 fn pp_function(func: &Function) -> RcDoc<'_> {
     let args = pp_args(&func.args);
-    let rets = pp_rets(&func.rets);
+    let rets = pp_args(&func.rets);
     let modes = pp_modes(&func.modes);
     sep_enclose([args, rets, modes], ",", "(", ")").nest(INDENT_SPACE)
 }
 
 fn pp_args(args: &[ArgType]) -> RcDoc<'_> {
-    let args = args.iter().map(|arg| pp_ty(&arg.typ));
-    sep_enclose(args, ",", "[", "]")
+    pp_types(args.iter().map(|arg| &arg.typ))
 }
 
-fn pp_rets(args: &[Type]) -> RcDoc<'_> {
-    sep_enclose(args.iter().map(pp_ty), ",", "[", "]")
+fn pp_types<'a, T>(types: T) -> RcDoc<'a>
+where
+    T: Iterator<Item = &'a Type>,
+{
+    sep_enclose(types.map(pp_ty), ",", "[", "]")
 }
 
 fn pp_modes(modes: &[candid::types::FuncMode]) -> RcDoc<'_> {
@@ -275,7 +277,7 @@ pub fn compile(env: &TypeEnv, actor: &Option<Type>) -> String {
                 .append(";");
 
             let idl_init_args = str("export const idlInitArgs = ")
-                .append(pp_rets(init_types))
+                .append(pp_types(init_types.iter()))
                 .append(";");
 
             let idl_factory_return = kwd("return").append(actor).append(";");
@@ -286,7 +288,9 @@ pub fn compile(env: &TypeEnv, actor: &Option<Type>) -> String {
             let init_defs = chase_types(env, init_types).unwrap();
             let init_recs = infer_rec(env, &init_defs).unwrap();
             let init_defs_doc = pp_defs(env, &init_defs, &init_recs, false);
-            let init_doc = kwd("return").append(pp_rets(init_types)).append(";");
+            let init_doc = kwd("return")
+                .append(pp_types(init_types.iter()))
+                .append(";");
             let init_doc = init_defs_doc.append(init_doc);
             let init_doc =
                 str("export const init = ({ IDL }) => ").append(enclose_space("{", init_doc, "};"));

rust/candid_parser/src/bindings/motoko.rs
@@ -212,16 +212,17 @@ fn pp_args(args: &[ArgType]) -> RcDoc<'_> {
     }
 }
 
-fn pp_rets(args: &[Type]) -> RcDoc<'_> {
-    match args {
+fn pp_rets(rets: &[ArgType]) -> RcDoc<'_> {
+    match rets {
         [ty] => {
-            if is_tuple(ty) {
-                enclose("(", pp_ty(ty), ")")
+            let typ = &ty.typ;
+            if is_tuple(typ) {
+                enclose("(", pp_ty(typ), ")")
             } else {
-                pp_ty(ty)
+                pp_ty(typ)
             }
         }
-        _ => sep_enclose(args.iter().map(pp_ty), ",", "(", ")"),
+        _ => sep_enclose(rets.iter().map(|ret| pp_ty(&ret.typ)), ",", "(", ")"),
     }
 }
 

rust/candid_parser/src/bindings/rust.rs
@@ -619,11 +619,11 @@ fn test_{test_name}() {{
         });
         sep_enclose(args, ",", "(", ")")
     }
-    fn pp_rets<'b>(&mut self, rets: &'b [Type], prefix: &'b str) -> RcDoc<'b> {
+    fn pp_rets<'b>(&mut self, rets: &'b [ArgType], prefix: &'b str) -> RcDoc<'b> {
         let tys = rets.iter().enumerate().map(|(i, t)| {
             let lab = format!("{prefix}{i}");
             let old = self.state.push_state(&StateElem::Label(&lab));
-            let res = self.pp_ty(t, true);
+            let res = self.pp_ty(&t.typ, true);
             self.state.pop_state(old, StateElem::Label(&lab));
             res
         });
@@ -701,10 +701,10 @@ fn test_{test_name}() {{
             .rets
             .iter()
             .enumerate()
-            .map(|(i, ty)| {
+            .map(|(i, ret)| {
                 let lab = format!("ret{i}");
                 let old = self.state.push_state(&StateElem::Label(&lab));
-                let res = self.pp_ty(ty, true);
+                let res = self.pp_ty(&ret.typ, true);
                 self.state.pop_state(old, StateElem::Label(&lab));
                 res
             })
@@ -1111,7 +1111,7 @@ impl<'b> NominalState<'_, 'b> {
                             .rets
                             .into_iter()
                             .enumerate()
-                            .map(|(i, ty)| {
+                            .map(|(i, ret)| {
                                 let lab = format!("ret{i}");
                                 let old = self.state.push_state(&StateElem::Label(&lab));
                                 let idx = if i == 0 {
@@ -1120,10 +1120,13 @@ impl<'b> NominalState<'_, 'b> {
                                     i.to_string()
                                 };
                                 path.push(TypePath::Func(format!("ret{idx}")));
-                                let ty = self.nominalize(env, path, &ty, None);
+                                let ty = self.nominalize(env, path, &ret.typ, None);
                                 path.pop();
                                 self.state.pop_state(old, StateElem::Label(&lab));
-                                ty
+                                ArgType {
+                                    name: ret.name.clone(),
+                                    typ: ty,
+                                }
                             })
                             .collect(),
                     })

rust/candid_parser/src/bindings/typescript.rs
@@ -207,9 +207,9 @@ fn pp_function<'a>(env: &'a TypeEnv, func: &'a Function) -> RcDoc<'a> {
     let args = sep_enclose(args, ",", "[", "]");
     let rets = match func.rets.len() {
         0 => str("undefined"),
-        1 => pp_ty(env, &func.rets[0], true),
+        1 => pp_ty(env, &func.rets[0].typ, true),
         _ => sep_enclose(
-            func.rets.iter().map(|ty| pp_ty(env, ty, true)),
+            func.rets.iter().map(|ty| pp_ty(env, &ty.typ, true)),
             ",",
             "[",
             "]",

rust/candid_parser/src/grammar.lalrpop
@@ -217,7 +217,7 @@ VariantFieldTyp: TypeField = {
     <doc_comment:DocComment> <id:FieldId> =>? Ok(TypeField { label: Label::Id(id), typ: IDLType::PrimT(PrimType::Null), docs: doc_comment.unwrap_or_default() }),
 }
 
-ArgTupTyp: Vec<IDLArgType> = "(" <SepBy<ArgTyp, ",">> ")" =>? {
+TupTyp: Vec<IDLArgType> = "(" <SepBy<ArgTyp, ",">> ")" =>? {
     let args = <>;
     let mut named_args: Vec<String> = args.iter().filter_map(|a| a.name.clone()).collect();
     named_args.sort();
@@ -225,10 +225,8 @@ ArgTupTyp: Vec<IDLArgType> = "(" <SepBy<ArgTyp, ",">> ")" =>? {
     Ok(args)
 };
 
-TupTyp: Vec<IDLType> = "(" <SepBy<Typ, ",">> ")" => <>;
-
 FuncTyp: FuncType = {
-    <args:ArgTupTyp> "->" <rets:TupTyp> <modes:FuncMode*> =>
+    <args:TupTyp> "->" <rets:TupTyp> <modes:FuncMode*> =>
         FuncType { modes, args, rets },
 }
 
@@ -271,7 +269,7 @@ Actor: IDLType = {
 
 MainActor: IDLActorType = {
     <doc_comment: DocComment> "service" "id"? ":" <t:Actor> ";"? => IDLActorType { typ: t, docs: doc_comment.unwrap_or_default() },
-    <doc_comment: DocComment> "service" "id"? ":" <args:ArgTupTyp> "->" <t:Actor> ";"? => IDLActorType { typ: IDLType::ClassT(args, Box::new(t)), docs: doc_comment.unwrap_or_default() },
+    <doc_comment: DocComment> "service" "id"? ":" <args:TupTyp> "->" <t:Actor> ";"? => IDLActorType { typ: IDLType::ClassT(args, Box::new(t)), docs: doc_comment.unwrap_or_default() },
 }
 
 pub IDLProg: IDLProg = {
@@ -279,7 +277,7 @@ pub IDLProg: IDLProg = {
 }
 
 pub IDLInitArgs: IDLInitArgs = {
-    <decs:SepBy<Def, ";">> <args:ArgTupTyp> => IDLInitArgs { decs, args }
+    <decs:SepBy<Def, ";">> <args:TupTyp> => IDLInitArgs { decs, args }
 }
 
 // Test file. Follows the "specification" in test/README.md

rust/candid_parser/src/syntax/mod.rs
@@ -51,7 +51,7 @@ impl std::str::FromStr for IDLType {
 
 #[derive(Debug, Clone)]
 pub struct IDLTypes {
-    pub args: Vec<IDLType>,
+    pub args: Vec<IDLArgType>,
 }
 
 impl std::str::FromStr for IDLTypes {
@@ -107,7 +107,7 @@ pub enum PrimType {
 pub struct FuncType {
     pub modes: Vec<FuncMode>,
     pub args: Vec<IDLArgType>,
-    pub rets: Vec<IDLType>,
+    pub rets: Vec<IDLArgType>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]

rust/candid_parser/src/syntax/pretty.rs
@@ -88,7 +88,7 @@ fn pp_function(func: &FuncType) -> RcDoc<'_> {
 
 fn pp_method(func: &FuncType) -> RcDoc<'_> {
     let args = pp_args(&func.args);
-    let rets = pp_rets(&func.rets);
+    let rets = pp_args(&func.rets);
     let modes = pp_modes(&func.modes);
     args.append(" ->")
         .append(RcDoc::space())
@@ -105,10 +105,6 @@ fn pp_args(args: &[IDLArgType]) -> RcDoc<'_> {
         }
     });
     sep_enclose(args, ",", "(", ")")
-}
-
-fn pp_rets(rets: &[IDLType]) -> RcDoc<'_> {
-    sep_enclose(rets.iter().map(pp_ty), ",", "(", ")")
 }
 
 fn pp_service(methods: &[Binding]) -> RcDoc<'_> {

rust/candid_parser/src/test.rs
@@ -1,5 +1,5 @@
 use super::typing::check_prog;
-use crate::syntax::{Dec, IDLProg, IDLType};
+use crate::syntax::{Dec, IDLArgType, IDLProg};
 use crate::{Error, Result};
 use candid::types::value::IDLArgs;
 use candid::types::{Type, TypeEnv};
@@ -7,7 +7,7 @@ use candid::DecoderConfig;
 
 const DECODING_COST: usize = 20_000_000;
 
-type TupType = Vec<IDLType>;
+type TupType = Vec<IDLArgType>;
 
 pub struct Test {
     pub defs: Vec<Dec>,
@@ -158,7 +158,7 @@ pub fn check(test: Test) -> Result<()> {
         print!("Checking {} {}...", i + 1, assert.desc());
         let mut types = Vec::new();
         for ty in assert.typ.iter() {
-            types.push(super::typing::ast_to_type(&env, ty)?);
+            types.push(super::typing::ast_to_type(&env, &ty.typ)?);
         }
         let input = assert.left.parse(&env, &types);
         let pass = if let Some(assert_right) = &assert.right {

rust/candid_parser/src/typing.rs
@@ -79,7 +79,7 @@ pub fn check_type(env: &Env, t: &IDLType) -> Result<Type> {
             }
             let mut t2 = Vec::new();
             for t in func.rets.iter() {
-                t2.push(check_type(env, t)?);
+                t2.push(check_arg(env, t)?);
             }
             if func.modes.len() > 1 {
                 return Err(Error::msg("cannot have more than one mode"));

tools/didc/src/main.rs
@@ -131,7 +131,7 @@ impl TypeAnnotation {
             (Some(tys), None) => {
                 let mut types = Vec::new();
                 for ty in tys.args.iter() {
-                    types.push(ast_to_type(&env, ty)?);
+                    types.push(ast_to_type(&env, &ty.typ)?);
                 }
                 Ok((env, types))
             }
@@ -139,10 +139,11 @@ impl TypeAnnotation {
                 let actor = actor
                     .ok_or_else(|| Error::msg("Cannot use --method with a non-service did file"))?;
                 let func = env.get_method(&actor, meth)?;
-                let types = match mode {
-                    Mode::Encode => func.args.iter().map(|arg| arg.typ.clone()).collect(),
-                    Mode::Decode => func.rets.clone(),
+                let types_iter = match mode {
+                    Mode::Encode => func.args.iter(),
+                    Mode::Decode => func.rets.iter(),
                 };
+                let types = types_iter.map(|arg| arg.typ.clone()).collect();
                 Ok((env, types))
             }
             _ => unreachable!(),

rust/candid_parser/tests/assets/ok/service.did
@@ -3,7 +3,7 @@ type Service2 = Service;
 type Func = func () -> (Service);
 service : {
   asArray : () -> (vec Service2, vec Func) query;
-  asPrincipal : () -> (Service2, Func);
+  asPrincipal : () -> (ret0 : Service2, ret1 : Func);
   asRecord : () -> (record { Service2; opt Service; Func });
   asVariant : () -> (variant { a : Service2; b : record { f : opt Func } });
 }

rust/candid_parser/tests/assets/service.did
@@ -5,5 +5,5 @@ service : {
   asArray: () -> (vec Service2, vec Func) query;
   asRecord: () -> (record { Service2; opt Service; Func });
   asVariant: () -> (variant { a: Service2; b: record { f:opt Func }});
-  asPrincipal: () -> (Service2, Func);
+  asPrincipal: () -> (ret0 : Service2, ret1 : Func);
 }

rust/candid_parser/tests/value.rs
@@ -66,10 +66,15 @@ service : {
     {
         let str = "(opt record { head = 1000; tail = opt record {head = -2000; tail = null}}, variant {a = 42})";
         let args = parse_idl_args(str).unwrap();
-        let encoded = args.to_bytes_with_types(&env, &method.rets).unwrap();
+        let rets = method
+            .rets
+            .iter()
+            .map(|ret| ret.typ.clone())
+            .collect::<Vec<_>>();
+        let encoded = args.to_bytes_with_types(&env, &rets).unwrap();
         let decoded = IDLArgs::from_bytes(&encoded).unwrap();
         assert_eq!(decoded.to_string(), "(\n  opt record {\n    1_158_359_328 = 1_000 : int16;\n    1_291_237_008 = opt record {\n      1_158_359_328 = -2_000 : int16;\n      1_291_237_008 = null;\n    };\n  },\n  variant { 97 = 42 : nat },\n)");
-        let decoded = IDLArgs::from_bytes_with_types(&encoded, &env, &method.rets).unwrap();
+        let decoded = IDLArgs::from_bytes_with_types(&encoded, &env, &rets).unwrap();
         assert_eq!(
             decoded.to_string(),
             "(\n  opt record {\n    head = 1_000 : int16;\n    tail = opt record { head = -2_000 : int16; tail = null };\n  },\n  variant { a = 42 : nat },\n)"