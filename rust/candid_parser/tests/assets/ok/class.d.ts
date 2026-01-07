01a068959aae246eec389aeab255a09d2247a608
Vivienne <vivienne.siffert@dfinity.org>
2026-01-07 13:08:28 +0100

    update test files

--- rust/candid_parser/tests/assets/ok/class.d.ts
+++ rust/candid_parser/tests/assets/ok/class.d.ts
@@ -1,6 +1,6 @@
-import type { Principal } from '@dfinity/principal';
-import type { ActorMethod } from '@dfinity/agent';
-import type { IDL } from '@dfinity/candid';
+import type { Principal } from '@icp-sdk/core/principal';
+import type { ActorMethod } from '@icp-sdk/core/agent';
+import type { IDL } from '@icp-sdk/core/candid';
 
 export type List = [] | [[bigint, List]];
 export interface Profile { 'age' : number, 'name' : string }
